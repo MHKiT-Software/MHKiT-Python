9a1dbedda6930cfc55655294d67f9b2c2ada3188
ssolson <ssolson@sandia.gov>
2020-03-05 11:59:36 -0700

    Added river usgs requst for instantaneous data

--- mhkit/tests/test_river.py
+++ mhkit/tests/test_river.py
@@ -239,7 +239,7 @@ class TestIO(unittest.TestCase):
         self.assertEqual(data.shape, (31, 1))
 
 
-    def test_request_usgs_data(self):
+    def test_request_usgs_data_daily(self):
         data=river.io.request_usgs_data(station="15515500",
                             parameter='00060',
                             start_date='2009-08-01',
@@ -247,8 +247,18 @@ class TestIO(unittest.TestCase):
                             data_type='Daily')
         self.assertEqual(data.columns, ['Discharge, cubic feet per second'])
         self.assertEqual(data.shape, (10, 1))
- 
-#        import ipdb; ipdb.set_trace() 
+    
+   
+    def test_request_usgs_data_instant(self):
+        data=river.io.request_usgs_data(station="15515500",
+                            parameter='00060',
+                            start_date='2009-08-01',
+                            end_date='2009-08-10',
+                            data_type='Instantaneous')
+        self.assertEqual(data.columns, ['Discharge, cubic feet per second'])
+        # Every 15 minutes or 4 times per hour
+        self.assertEqual(data.shape, (10*24*4, 1))
+
 
 if __name__ == '__main__':
     unittest.main() 
