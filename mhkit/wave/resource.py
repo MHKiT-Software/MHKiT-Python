92263a928c7959ba8d19af9db39c12d8bd812379
ssolson <ssolson@sandia.gov>
2020-05-07 11:55:53 -0600

    Fix DocString. Added noverlap description to elevation spectrum docstring

--- mhkit/wave/resource.py
+++ mhkit/wave/resource.py
@@ -25,7 +25,10 @@ def elevation_spectrum(eta, sample_rate, nnft, window='hann', detrend=True, nove
     detrend: bool (optional)
         Specifies if a linear trend is removed from the data before calculating 
         the wave energy spectrum.  Data is detrended by default.
-        
+    noverlap: int, optional
+        Number of points to overlap between segments. If None,
+        ``noverlap = nperseg / 2``.  Defaults to None. 
+                
     Returns
     ---------
     S: pandas DataFrame
