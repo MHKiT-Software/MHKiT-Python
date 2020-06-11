653ef9b51276fe6fcad7dd02d3f245fd222952f5
Ivanov <hivanov@nrel.gov>
2020-06-11 13:08:05 -0600

    initial test util functions

--- mhkit/tests/test_utils.py
+++ mhkit/tests/test_utils.py
@@ -1,4 +1,70 @@
 import unittest
 import numpy as np
+import pandas as pd
 import mhkit.utils as utils
+from pandas.testing import assert_frame_equal
+
+class TestGenUtils(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.filepath = 'data/data_loads.csv'
+        self.freq = 50 # Hz
+        self.period = 600 # seconds
+
+    def test_get_stats(self):
+        # load in file
+        df = pd.read_csv(self.filepath)
+        df.Timestamp = pd.to_datetime(df.Timestamp)
+        df.set_index('Timestamp',inplace=True)
+        # run function
+        means,maxs,mins,stdevs = utils.get_stats(df,self.freq,period=self.period)
+        # check statistics
+        self.assertAlmostEqual(means['uWind_80m'],7.773,2) # mean
+        self.assertAlmostEqual(maxs['uWind_80m'],13.271,2) # max
+        self.assertAlmostEqual(mins['uWind_80m'],3.221,2) # min
+        self.assertAlmostEqual(stdevs['uWind_80m'],1.551,2) # standard deviation
+        # check timestamp
+        string_time = '2017-03-01 01:28:41'
+        time = pd.to_datetime(string_time)
+        self.assertTrue(means.index[0]==time)
+        
+    def test_uwrapvec(self):
+        # create array of test values and corresponding expected answers
+        test = [-740,-400,-50,0,50,400,740]
+        correct = [340,320,310,0,50,40,20]
+        # get answers from function
+        answer = utils.unwrapvec(test)
+        
+        # check if answer is correct
+        assert_frame_equal(pd.DataFrame(answer),pd.DataFrame(correct))
+
+    def test_excel2datetime(self):
+        # store excel timestamp
+        excel_time = 4.279549212962963e+04
+        # corresponding datetime
+        string_time = '2017-03-01 11:48:40'
+        time = pd.to_datetime(string_time)
+        # test function
+        answer = utils.excel2datetime(excel_time)
+        answer2 = answer.round('s') # round to nearest second for comparison
+        
+        # check if answer is correct
+        self.assertTrue(answer2 == time)
+
+    def test_matlab2datetime(self):
+        # store excel timestamp
+        mat_time = 7.367554921296296e+05
+        # corresponding datetime
+        string_time = '2017-03-01 11:48:40'
+        time = pd.to_datetime(string_time)
+        # test function
+        answer = utils.matlab2datetime(mat_time)
+        answer2 = answer.round('s') # round to nearest second for comparison
+        
+        # check if answer is correct
+        self.assertTrue(answer2 == time)
+
+
+
 
