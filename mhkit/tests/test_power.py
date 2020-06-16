af217bef666c3cd5ac0432cee925af466b0d6ccf
rpauly18 <rpauly@nrel.gov>
2020-06-16 12:23:22 -0600

    adding more power.quality tests

--- mhkit/tests/test_power.py
+++ mhkit/tests/test_power.py
@@ -16,9 +16,12 @@ class TestDevice(unittest.TestCase):
         self.frequency = 60
         self.freq_array = np.ones(len(self.samples))*60
         harmonics_int = np.arange(0,60*50,5)
+        self.interharmonic = np.zeros(len(harmonics_int)) #since this is an idealized sin wave, the interharmonics should be zero
         self.harmonics_vals = np.zeros(len(harmonics_int))
-        self.harmonics_vals[12]= 1.0  
-        print(self.harmonics_vals)
+        self.harmonics_vals[12]= 1.0  #setting 60th harmonic to amplitude of the signal
+        self.harmonic_groups = self.harmonics_vals[0::12] #harmonic groups should be equal to every 12th harmonic in this idealized example
+        self.thcd = 0.0 #Since this is an idealized sin wave, there should be no distortion 
+        
         self.signal = np.sin(2 * np.pi * self.frequency * self.samples)
         
         self.current_data = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
@@ -41,9 +44,22 @@ class TestDevice(unittest.TestCase):
         current = pd.Series(self.signal,index = self.samples)
         harmonics = power.quality.harmonics(current,1000,self.frequency)
         for i,j in zip(harmonics.values,self.harmonics_vals):
-            print(i)
-            print(j)
+            
             self.assertAlmostEqual(i[0], j,1)
+        hsg = power.quality.harmonic_subgroups(harmonics,self.frequency)
+        for i,j in zip(hsg.values,self.harmonic_groups):
+            
+            self.assertAlmostEqual(i[0], j,1)
+
+        TCHD = power.quality.total_harmonic_current_distortion(hsg,18.8) # had to just put a random rated current in here
+        self.assertAlmostEqual(TCHD.values[0],self.thcd)
+        #test interharmonics
+        ih = power.quality.interharmonics(harmonics,self.frequency)
+        for i,j in zip(ih.values,self.interharmonic):
+            
+            self.assertAlmostEqual(i[0], j,1)
+
+
         
 
     def test_dc_power_DataFrame(self):
