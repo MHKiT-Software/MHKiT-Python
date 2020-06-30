0bdd9044faba50bcc6e1803c9941e69fafe5c13b
Ivanov <hivanov@nrel.gov>
2020-06-30 15:45:54 -0600

    bug fixes from json data storage change

--- mhkit/tests/test_utils.py
+++ mhkit/tests/test_utils.py
@@ -3,27 +3,41 @@ import numpy as np
 import pandas as pd
 import mhkit.utils as utils
 from pandas.testing import assert_frame_equal
+import json
+from os.path import abspath, dirname, join, isfile
+
+testdir = dirname(abspath(__file__))
+datadir = join(testdir, 'data')
 
 class TestGenUtils(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        self.filepath = 'data/data_loads.csv'
+        loads_data_file = join(datadir, "loads_data_dict.json")
+        with open(loads_data_file, 'r') as fp:
+            data_dict = json.load(fp)
+        # convert dictionaries into dataframes
+        data = {
+                key: pd.DataFrame(data_dict[key]) 
+                for key in data_dict
+               }
+        self.data = data
+
         self.freq = 50 # Hz
         self.period = 600 # seconds
 
     def test_get_statistics(self):
         # load in file
-        df = pd.read_csv(self.filepath)
+        df = self.data['loads']
         df.Timestamp = pd.to_datetime(df.Timestamp)
         df.set_index('Timestamp',inplace=True)
         # run function
         means,maxs,mins,stdevs = utils.get_statistics(df,self.freq,period=self.period)
         # check statistics
-        self.assertAlmostEqual(means['uWind_80m'],7.773,2) # mean
-        self.assertAlmostEqual(maxs['uWind_80m'],13.271,2) # max
-        self.assertAlmostEqual(mins['uWind_80m'],3.221,2) # min
-        self.assertAlmostEqual(stdevs['uWind_80m'],1.551,2) # standard deviation
+        self.assertAlmostEqual(means.reset_index().loc[0,'uWind_80m'],7.773,2) # mean
+        self.assertAlmostEqual(maxs.reset_index().loc[0,'uWind_80m'],13.271,2) # max
+        self.assertAlmostEqual(mins.reset_index().loc[0,'uWind_80m'],3.221,2) # min
+        self.assertAlmostEqual(stdevs.reset_index().loc[0,'uWind_80m'],1.551,2) # standard deviation
         # check timestamp
         string_time = '2017-03-01 01:28:41'
         time = pd.to_datetime(string_time)
@@ -37,7 +51,7 @@ class TestGenUtils(unittest.TestCase):
         answer = utils.unwrap_vector(test)
         
         # check if answer is correct
-        assert_frame_equal(pd.DataFrame(answer),pd.DataFrame(correct))
+        assert_frame_equal(pd.DataFrame(answer,dtype='int32'),pd.DataFrame(correct,dtype='int32'))
 
     def test_matlab_to_datetime(self):
         # store matlab timestamp
