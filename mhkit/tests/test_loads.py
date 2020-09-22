861fc46987c17a4a84eefe42457379f95ccba6a0
Ivanov <hivanov@nrel.gov>
2020-09-22 10:40:27 -0600

    loads module reorg

--- mhkit/tests/test_loads.py
+++ mhkit/tests/test_loads.py
@@ -1,7 +1,7 @@
 from os.path import abspath, dirname, join, isfile, normpath, relpath
 from pandas.testing import assert_frame_equal
 from mhkit import utils
-from mhkit import loads
+import mhkit.loads as loads
 import pandas as pd 
 import numpy as np
 import unittest
@@ -43,69 +43,40 @@ class TestLoads(unittest.TestCase):
         # Apply function to calculate means
         load_means =self.data['means']
         bin_against = load_means['uWind_80m']
-        [b_means, b_means_std] = loads.bin_statistics(load_means, bin_against, bin_edges)
+        [b_means, b_means_std] = loads.general.bin_statistics(load_means, bin_against, bin_edges)
 
         assert_frame_equal(self.data['bin_means'],b_means)
         assert_frame_equal(self.data['bin_means_std'],b_means_std)
-
-
-    def test_tip_speed_ratio(self):
-        rotor_speed = [15,16,17,18] # create array of rotor speeds
-        rotor_diameter = 77 # diameter of rotor for GE 1.5
-        inflow_speed = [13,13,13,13] # array of wind speeds
-        TSR_answer = [4.7,5.0,5.3,5.6]
-        
-        TSR = loads.tip_speed_ratio(np.asarray(rotor_speed)/60,rotor_diameter,inflow_speed)
-
-        for i,j in zip(TSR,TSR_answer):
-            self.assertAlmostEqual(i,j,delta=0.05)
-
-
-    def test_power_coefficient(self):
-        # data obtained from power performance report of wind turbine
-        inflow_speed = [4,6,8,10,12,14,16,18,20]
-        power_out = np.asarray([59,304,742,1200,1400,1482,1497,1497,1511])
-        capture_area = 4656.63
-        rho = 1.225
-        Cp_answer = [0.320,0.493,0.508,0.421,0.284,0.189,0.128,0.090,0.066]
-        
-        Cp = loads.power_coefficient(power_out*1000,inflow_speed,capture_area,rho)
-
-        for i,j in zip(Cp,Cp_answer):
-            self.assertAlmostEqual(i,j,places=2)
-
-
+  
     def test_blade_moments(self):
         flap_raw = self.blade_data['flap_raw']
         flap_offset = self.flap_offset
         edge_raw = self.blade_data['edge_raw']
         edge_offset = self.edge_offset
 
-        M_flap, M_edge = loads.blade_moments(self.blade_matrix,flap_offset,flap_raw,edge_offset,edge_raw)
+        M_flap, M_edge = loads.general.blade_moments(self.blade_matrix,flap_offset,flap_raw,edge_offset,edge_raw)
 
         for i,j in zip(M_flap,self.blade_data['flap_scaled']):
             self.assertAlmostEqual(i,j,places=1)
         for i,j in zip(M_edge,self.blade_data['edge_scaled']):
             self.assertAlmostEqual(i,j,places=1)
 
-
     def test_damage_equivalent_loads(self):
         loads_data = self.data['loads']
         tower_load = loads_data['TB_ForeAft']
         blade_load = loads_data['BL1_FlapMom']
-        DEL_tower = loads.damage_equivalent_load(tower_load, 4,bin_num=100,data_length=600)
-        DEL_blade = loads.damage_equivalent_load(blade_load,10,bin_num=100,data_length=600)
+        DEL_tower = loads.general.damage_equivalent_load(tower_load, 4,bin_num=100,data_length=600)
+        DEL_blade = loads.general.damage_equivalent_load(blade_load,10,bin_num=100,data_length=600)
 
         self.assertAlmostEqual(DEL_tower,self.fatigue_tower,delta=self.fatigue_tower*0.04)
         self.assertAlmostEqual(DEL_blade,self.fatigue_blade,delta=self.fatigue_blade*0.04)
-
 
     def test_plot_statistics(self):
         # Define path
         savepath = abspath(join(testdir, 'test_scatplotter.png'))
         
         # Generate plot
-        loads.plot_statistics( self.data['means']['uWind_80m'],
+        loads.graphics.plot_statistics( self.data['means']['uWind_80m'],
                                self.data['means']['TB_ForeAft'],
                                self.data['maxs']['TB_ForeAft'],
                                self.data['mins']['TB_ForeAft'],
@@ -115,7 +86,6 @@ class TestLoads(unittest.TestCase):
                                save_path=savepath)
         
         self.assertTrue(isfile(savepath))
-
 
     def test_plot_bin_statistics(self):
         # Define signal name, path, and bin centers
@@ -132,7 +102,7 @@ class TestLoads(unittest.TestCase):
         bin_min_std = self.data['bin_mins_std'][signal_name]
 
         # Generate plot
-        loads.plot_bin_statistics(bin_centers, 
+        loads.graphics.plot_bin_statistics(bin_centers, 
                                   bin_mean, bin_max, bin_min,
                                   bin_mean_std, bin_max_std, bin_min_std,
                                   x_label='Wind Speed [m/s]',
@@ -141,6 +111,7 @@ class TestLoads(unittest.TestCase):
                                   save_path=savepath)
 
         self.assertTrue(isfile(savepath))
+
 
 if __name__ == '__main__':
     unittest.main()
