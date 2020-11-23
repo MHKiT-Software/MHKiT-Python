04efaf0115f653d9ee0b2d2c9b9b329e804cc028
rpauly18 <rpauly@nrel.gov>
2020-11-23 12:05:40 -0700

    trying to fix build issue again

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ DEPENDENCIES = ['pandas>=1.0.0',
                 'fatpack',
                 'lxml',
                 'scikit-learn',
-		'NREL-rex>=0.2.30',
-        'six>=1.13.0']
+		        'NREL-rex>=0.2.30',
+                'six>=1.13.0']
 
 # use README file as the long description
 file_dir = os.path.abspath(os.path.dirname(__file__))
