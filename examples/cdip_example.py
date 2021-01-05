442e081ed32601ae2bca975cc77ce03659d5abd6
ssolson <ssolson@sandia.gov>
2021-01-05 16:02:23 -0700

    Adjust to work with new function name, kwargs ect.

--- examples/cdip_example.py
+++ examples/cdip_example.py
@@ -1,31 +1,32 @@
+from mhkit.wave.io import cdip
+import matplotlib.pyplot as plt
 import mhkit 
 
 ########################################################################
 # Boxplot Exampe
 ########################################################################
 # Load Historic data
-stn = '067'
+station_number = '067'
 year_date = '2011'
 
-[data, buoytitle] = mhkit.wave.io.cdip.request_data(stn,'','',year_date)
-data.head()
+data = cdip.request_historic(station_number,year=year_date)
 
 # Plot Boxplot
-mhkit.wave.graphics.plot_boxplot(data, buoytitle)
+mhkit.wave.graphics.plot_boxplot(data, data.name)
 #Kelley FIX 
 # Boxplot scatter plot of means not properly alligned
 # Boxplot doens't matach CDIP example
-
+import ipdb; ipdb.set_trace()
 ########################################################################
-# Compendium Exampe
+# Compendium Example
 ########################################################################
 
 #########################
 # Load Historic data
 #########################
-# stn = '100'
-# start_date = "04/01/2012" # MM/DD/YYYY
-# end_date = "04/30/2012"
+stn = '100'
+start_date = "04/01/2012" # MM/DD/YYYY
+end_date = "04/30/2012"
 
 # stn = '179'
 # start_date = "04/01/2019" # MM/DD/YYYY
@@ -39,7 +40,7 @@ mhkit.wave.graphics.plot_boxplot(data, buoytitle)
 # start_date = "08/01/2018" # MM/DD/YYYY
 # end_date = "08/31/2018"
 
-# [data, buoytitle] = mhkit.wave.io.cdip.request_data(stn,start_date,end_date,'')
+data = cdip.request_historic(stn,start_date= start_date,end_date=start_date)
 # data.head()
 
 #########################
@@ -55,6 +56,8 @@ mhkit.wave.graphics.plot_boxplot(data, buoytitle)
 #########################
 # Plot data Compendium
 #########################
-# mhkit.wave.graphics.plot_compendium(data, buoytitle)
+mhkit.wave.graphics.plot_compendium(data, data.name)
+
+import ipdb; ipdb.set_trace()
 
 
