212baddcb0b2ee6708ae8851d64f2a09606627f9
rpauly18 <rpauly@nrel.gov>
2021-01-07 13:28:01 -0700

    moving hindcast data location

--- mhkit/tests/test_wave.py
+++ mhkit/tests/test_wave.py
@@ -775,29 +775,29 @@ class TestWPTOhindcast(unittest.TestCase):
     @classmethod
     def setUpClass(self):
 
-        self.my_swh = pd.read_csv(join(datadir,'multi_year_hindcast.csv'),index_col = 'time_index',
+        self.my_swh = pd.read_csv(join(datadir,'hindcast/multi_year_hindcast.csv'),index_col = 'time_index',
         names = ['time_index','significant_wave_height_0'],header = 0, 
         dtype = {'significant_wave_height_0':'float32'})
         self.my_swh.index = pd.to_datetime(self.my_swh.index)
 
-        self.ml = pd.read_csv(join(datadir,'single_year_hindcast_multiloc.csv'),index_col = 'time_index',
+        self.ml = pd.read_csv(join(datadir,'hindcast/single_year_hindcast_multiloc.csv'),index_col = 'time_index',
         names = ['time_index','mean_absolute_period_0','mean_absolute_period_1'],
         header = 0, dtype = {'mean_absolute_period_0':'float32',
         'mean_absolute_period_1':'float32'})
         self.ml.index = pd.to_datetime(self.ml.index)
 
-        self.mp = pd.read_csv(join(datadir,'multiparm.csv'),index_col = 'time_index',
+        self.mp = pd.read_csv(join(datadir,'hindcast/multiparm.csv'),index_col = 'time_index',
         names = ['time_index','omni-directional_wave_power_0','energy_period_0'],
         header = 0, dtype = {'omni-directional_wave_power_0':'float32',
         'energy_period_0':'float32'})
         self.mp.index = pd.to_datetime(self.mp.index)
 
-        self.ml_meta = pd.read_csv(join(datadir,'multiloc_meta.csv'),index_col = 0,
+        self.ml_meta = pd.read_csv(join(datadir,'hindcast/multiloc_meta.csv'),index_col = 0,
         names = [None,'water_depth','latitude','longitude','distance_to_shore','timezone'
         ,'jurisdiction'],header = 0, dtype = {'water_depth':'float32','latitude':'float32'
         ,'longitude':'float32','distance_to_shore':'float32','timezone':'int16'})
 
-        self.metadata = pd.read_csv(join(datadir,'meta.csv'),index_col = 0,
+        self.metadata = pd.read_csv(join(datadir,'hindcast/meta.csv'),index_col = 0,
         names = [None,'water_depth','latitude','longitude','distance_to_shore','timezone'
         ,'jurisdiction'],header = 0, dtype = {'water_depth':'float32','latitude':'float32'
         ,'longitude':'float32','distance_to_shore':'float32','timezone':'int16'})
