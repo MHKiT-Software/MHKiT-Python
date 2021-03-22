f45c3f675d3f6cdbaede31680dd7450216693ee3
ssolson <ssolson@sandia.gov>
2021-03-22 10:13:52 -0600

    1 test (multi_param), no wait

--- mhkit/tests/test_wave.py
+++ mhkit/tests/test_wave.py
@@ -815,17 +815,17 @@ class TestWPTOhindcast(unittest.TestCase):
 
     ### WPTO hindcast data
 
-    def test_multi_year_sig_wave_height(self):
+    # def test_multi_year_sig_wave_height(self):
     
-        data_type = '3-hour'
-        years = [1990,1992]
-        lat_lon = (44.624076,-124.280097) 
-        parameters = 'significant_wave_height'
-        value = randint(300,500)
-        #time.sleep(value)
-        wave_multiyear, meta = wave.io.hindcast.request_wpto_point_data(data_type,parameters,lat_lon,years)
-        assert_frame_equal(self.my_swh,wave_multiyear)
-        assert_frame_equal(self.my_meta,meta)
+        # data_type = '3-hour'
+        # years = [1990,1992]
+        # lat_lon = (44.624076,-124.280097) 
+        # parameters = 'significant_wave_height'
+        # value = randint(300,500)
+        # #time.sleep(value)
+        # wave_multiyear, meta = wave.io.hindcast.request_wpto_point_data(data_type,parameters,lat_lon,years)
+        # assert_frame_equal(self.my_swh,wave_multiyear)
+        # assert_frame_equal(self.my_meta,meta)
 
 
     # def test_multi_loc(self):
@@ -848,7 +848,7 @@ class TestWPTOhindcast(unittest.TestCase):
         years = [1996]
         lat_lon = (44.624076,-124.280097) 
         parameters = ['energy_period','mean_zero-crossing_period']
-        value = randint(300,500)
+        #value = randint(300,500)
         time.sleep(value)
         wave_multiparm, meta= wave.io.hindcast.request_wpto_point_data(data_type,
         parameters,lat_lon,years)
