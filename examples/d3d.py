ce874e50cd2cecb4cb4e82b755b3e9064e98dcc8
browniea <eabrowning@alaska.edu>
2021-04-29 10:39:28 -0800

    changed d3d.irregualar_grids to d3d.py

--- examples/d3d.py
+++ examples/d3d.py
@@ -6,51 +6,10 @@ import netCDF4
 import pandas as pd
 
 
-exdir= dirname(abspath(__file__))
-datadir = normpath(join(exdir,relpath('data/river/d3d')))
-filename= 'turbineTest_map.nc'
-data = netCDF4.Dataset(join(datadir,filename))
 
-def plot_variable(data,variable, layer, TS=-1):
-
+def _get_layer_data(data, variable, layer = -1 , time_step=-1):
     '''
-    plots velocity for d3d map file 
-
-    Parameters
-    ----------
-    data : netcdf4 object 
-        d3d netcdf file 
-    variable : string
-        variable to call. 
-    TS: float 
-        time step. Defalt is late tiem step -1  
-    Layer: float
-        Delft3D layer.      
-        
-    Returns
-    -------
-    ax: figure 
-        contour of velocity
-
-    '''
-    x,y,z= get_variable(data,variable,layer, TS)
-
-    
-    plt.tricontourf(x,y,z)
-    plt.title(f'Layer {layer}')
-    cbar=plt.colorbar()
-    units= data.variables[variable].units
-    cname=data.variables[variable].long_name
-    cbar.set_label(f'{cname} [{units}]')
-    plt.xlabel('x (m)')
-    plt.ylabel('y (m)')
-    plt.show()
-
-
-
-def get_variable(data,variable,layer = -1 ,TS=-1):
-    '''
-    get variable data from netcdf4 object
+    Get variable data from netcdf4 object at specified layer and timestep. 
 
     Parameters
     ----------
@@ -58,237 +17,223 @@ def get_variable(data,variable,layer = -1 ,TS=-1):
         d3d netcdf file 
     variable : string
         variable to call.
-    TS: float 
-        time step. Defalt is late tiem step -1  
     Layer: float
         Delft3D layer. layer must be a positve interger 
+    TS: float 
+        time step. Defalt is late tiem step -1  
+
 
     Returns
     -------
-    None.
+    x,y,v: float
+        "x" and "y" location on specified layer with the variables values "v"
 
     '''
     coords = str(data.variables[variable].coordinates).split()
     var=data.variables[variable][:]
     max_layer= len(var[0][0])
-    itime= TS 
-    x=data.variables[coords[0]][:]
-    y=data.variables[coords[1]][:]
+    itime= time_step
+    x=np.ma.getdata(data.variables[coords[0]][:], False) 
+    y=np.ma.getdata(data.variables[coords[1]][:], False)
     if layer > max_layer:
         print ('layer out of range')
-        z=[0]*len(x)
+        v=[0]*len(x)
     else:
-        z= var[itime,:,layer]
-    return x,y,z
-#==============================================================================
+        v= np.ma.getdata(var[itime,:,layer], False)
+    return x,y,v
 
-def x_y_interpolation(x, y, v,layer, TS=-1, center_line=3): 
+
+
+def create_points( x, y, z):
+
     '''
+    Turns x, y, and z into a DataFrame of points to interpolate over,
+    Must be provided at least two points and at most one array 
+    
     Parameters
     ----------
-    x : location 
-        get_variable output, float array 
-    y : location 
-        get_variable output, float array 
-    v : values
-        get_variable output, float array 
-    layer : layer of data 
-        get_variable input, float arrayu 
-    TS : timestep 
-        float. The default is -1.
-    center_line : desired y value 
-        DESCRIPTION. The default is 3.
+   
+    x: float, array or int 
+        x values to create points
+    y: float, array or int
+        y values to create points
+    z: float, array or int
+        z values to create points
 
     Returns
     -------
-    x_plot, v_plot 
+    points: DateFrame 
+        DataFrame of x, y and z points 
 
-   '''
-   
-    df = pd.DataFrame(x, columns=['x'])
-    df['y'] = y
-    df['v'] = v
-    
-    y_unique= np.unique(y)
-    #x_unique=np.unique(x)
-    
-    imax = np.searchsorted(y_unique, center_line)
-    imin=imax-1
-        
-    if imax== 0 or imin == y_unique[-1] :
-        print('error')
-    else:
-                
-        ymin = y_unique[imin]
-        ymax = y_unique[imax]
-            
-        y_mins = df[df.y==ymin].y
-        y_maxs = df[df.y==ymax].y
-        Y=np.concatenate((y_mins.values, y_maxs.values))
-            
-                            
-        x_mins = df[df.y==ymin].x
-        x_maxs = df[df.y==ymax].x
-        X=np.concatenate((x_mins.values, x_maxs.values))
-                
-            
-        v_mins = df[df.y==ymin].v
-        v_maxs = df[df.y==ymax].v
-        V=np.concatenate((v_mins.values, v_maxs.values))
-    
-        points= np.concatenate((X.reshape(-1,1), Y.reshape(-1,1), V.reshape(-1,1)), axis=1)
-        points= pd.DataFrame(points, columns=['X','Y','V'])
-        points_sorted = points.sort_values(by= 'X')
-        points_sorted = points_sorted.reset_index(drop=True)
-            
-            
-        X_interpolated_centerline= []
-        V_interpolated_centerline= []
-            
-        
-    
-        for i in range(len(points_sorted)-1):
-            X = np.interp(center_line, (points_sorted.Y[i],points_sorted.Y[i+1]), (points_sorted.X[i],points_sorted.X[i+1]))
-            V = np.interp(center_line, (points_sorted.Y[i],points_sorted.Y[i+1]), (points_sorted.V[i],points_sorted.V[i+1]))                    
-            X_interpolated_centerline.append(X)
-            V_interpolated_centerline.append(V)
-                
-        x_plot = X_interpolated_centerline
-        v_plot = V_interpolated_centerline
-    
-    
-    return x_plot, v_plot 
-    
-##################################################
-
-vars= ['ucx', 'turkin1']
-for var in vars: 
-   plot_variable(data,var, 4)
-   
-plt.show()
-#plot_variable(data, 'ucx', 'velocity', 3)
-
-# interpolate turbulence data onto velocity grid 
-#x,y,z= get_variable(data,'ucx')
-#x2,y2,z2= get_variable(data,'turkin1')
-
-#z3 = interp.griddata(np.array([x,y]).T, z,(x2, y2), method='cubic')
-#plt.tricontourf(x2,y2,z3)
-#plt.show()
-
-# Turblent intensity
-
-# x,y,vx= get_variable(data,'ucx',2)
-# x,y,vy= get_variable(data,'ucy',2)
-# x,y,vz= get_variable(data,'ucz',2)
-# x,y,TK= get_variable(data,'turkin1',2)
-
-# mv= (vx**2+vy**2+vz**2)**0.5
-# TI= (2/3*TK)**0.5/mv 
-
-
-
-#Centerline Plot 
-def plot_centerline(data,variable,depth, TS=-1, center_line=3):
     '''
+    directions = {0: {'name' : 'x',
+                     'values': x},
+                 1:{'name' : 'y',
+                    'values' : y},
+                 2:{'name' : 'z',
+                    'values' : z}}
+
+    for i in directions:
+        try:
+            len(directions[i]['values'])
+        except:
+            directions[i]['values'] = np.array([directions[i]['values']])  
+        N= len(directions[i]['values'])
+        if N== 1 :
+            directions[i]['type']= 'point'
+        elif N > 1 :
+            directions[i]['type']= 'vector'
+        else:
+            raise Exception(f'length of direction {directions[i]["name"]} was neagative or zero')
+    
+    types= [directions[i]['type'] for i in directions]
+    #check how many times point is in "types" 
+    N_points = types.count('point')
+    if N_points >= 2: 
+        #  treat_as centerline 
+        lens = np.array([np.size(d)  for d in directions])
+        max_len_idx = lens.argmax()
+        not_max_idxs= [i for i in directions.keys()]
+        del not_max_idxs[max_len_idx]
+                
+                    
+        for not_max in not_max_idxs:     
+            N= len(directions[max_len_idx]['values'])
+            vals =np.ones(N)*directions[not_max]['values']
+            directions[not_max]['values'] = np.array(vals)
+                    
+        x_new = directions[0]['values']
+        y_new = directions[1]['values']
+        z_new = directions[2]['values']
+            
+        request= np.array([ [x_i, y_i, z_i] for x_i, y_i, z_i in zip(x_new, y_new, z_new)]) 
+        points= pd.DataFrame(request, columns=[ 'x', 'y', 'z'])
+    elif N_points ==1: 
+        # treat as plane
+        #find inded of point 
+        idx_point = types.index('point')
+        max_idxs= [i for i in directions.keys()]
+        del max_idxs[idx_point]
+        #find vectors 
+        # 
+        XX, YY = np.meshgrid(directions[max_idxs[0]]['values'], directions[max_idxs[1]]['values'] )
+        N_X=np.shape(XX)[1]#or take len of original vectors 
+        N_Y=np.shape(YY)[0]
+        
+        ZZ= np.ones((N_Y,N_X))*directions[idx_point]['values'] #make sure is the same shape as XX , YY 
+     
+        request= np.array([ [x_i, y_i, z_i] for x_i, y_i, z_i in zip(XX.ravel(),
+                            YY.ravel() , ZZ.ravel())]) 
+        columns=[ directions[max_idxs[0]]['name'],  
+                 directions[max_idxs[1]]['name'],  directions[idx_point]['name']]
+        
+        points= pd.DataFrame(request, columns=columns)
+    else: 
+        raise Exception('Can provide at most two vectors')
+    
+
+
+    return points
+ 
+
+
+def get_all_data_points(data, variable, time_step):
+    '''
+    Get data points from all layers in netcdf file generated from Delft3D  
+
+    Parameters
+    ----------
+    data : netcdf object 
+        d3d netcdf object 
+    variable : string
+        string to call variable 
+    time_step : float 
+        time step
+
+    Returns
+    -------
+    all_data: DataFrame 
+        Data frame of x , y, z, and variable 
+
+    '''
+ # TODO loop between Velocity and turbulence data 
+    Layer_percentages= np.ma.getdata(data.variables['LayCoord_cc'][:], False) # velocity data 
+   # Layer_percentages= np.ma.getdata(data.variables['LayCoord_w'][:], False) # turbulent data 
+    
+    bottom_depth=np.ma.getdata(data.variables['waterdepth'][time_step, :], False)# add Time_step
+    
+    x_all=[]
+    y_all=[]
+    z_all=[]
+    v_all=[]
+    
+    N_layers = range(len(Layer_percentages))
+    
+    for layer in N_layers:
+        x,y,v= _get_layer_data(data, variable, layer, time_step)
+        z = [bottom_depth*Layer_percentages[layer]]
+        x_all=np.append(x_all, x)
+        y_all=np.append(y_all, y)
+        z_all=np.append(z_all, z)
+        v_all=np.append(v_all, v)
+    
+    known_points = np.array([ [x, y, z, v] for x, y, z, v in zip(x_all, y_all, z_all, v_all)])
+    
+    all_data= pd.DataFrame(known_points, columns=['x','y','z',f'{variable}'])
+    
+    return all_data
+
+def interpolate_data(data_points, values , request_points):
+   
+    '''
+    interpolate for "requested_points" vales over the known "data_points" wiuth corresponding "values" 
     Parameters
     ----------
     data : netcdf4 object 
         d3d netcdf file 
-    variable : string
-        variable to call.
-    depth: float
-        Delft3D layer.      
-    TS: float 
-        time step. Defalt is late tiem step -1  
-    center_line : float, optional
-        centerline location. The default is 3.
+    points: DataFrame 
+        Data Frame of x, y, z, points 
+    request_points: DataFrame 
+        x, y and z, locations of points calculate the values 
+
     Returns
     -------
-    None.
+    v_new: array 
+        interpolared values for locations in request_points 
 
     '''
-    lower_layer= round(np.floor(depth))
+    v_new = interp.griddata(data_points, values, request_points)
 
-    upper_layer= round(lower_layer + 1) 
+    return v_new
 
-    if depth == lower_layer:
-        layer= depth
-        x,y,v= get_variable(data, variable, layer,TS)
-            
-        x = np.ma.getdata(x, False)
-        y = np.ma.getdata(y, False)
-        v = np.ma.getdata(v, False)
-                
-        df = pd.DataFrame(x, columns=['x'])
-        df['y'] = y
-        df['v'] = v
-                
-        y_unique= np.unique(y)
-        x_unique= np.unique(x)
-                  
-        #import ipdb; ipdb.set_trace()
-        if  any(center_line==y_unique):
-            Yidx=len(y_unique)//2
-            idx= y_unique[Yidx]
-            center_line_index = np.where(y== idx)
-            x_plot = x[center_line_index]
-            v_plot = v[center_line_index]
-                    
-         
-        else: 
-            imax = np.searchsorted(y_unique, center_line)
-            imin=imax-1
-            
-            if imax== 0 or imin == y_unique[-1] :
-                print('error')
-            else:
 
-               x_plot, v_plot = x_y_interpolation( x, y, v, layer, TS, center_line) 
-        
-
-    else:
-        
-        x_upper, y_upper, v_upper = get_variable(data, variable,upper_layer,TS)
-        x_upper = np.ma.getdata(x_upper, False)
-        y_upper = np.ma.getdata(y_upper, False)
-        v_upper = np.ma.getdata(v_upper, False)
-        
     
-        x_plot_upper, v_plot_upper = x_y_interpolation(x_upper, y_upper, v_upper, upper_layer,TS, center_line) 
-        
-        
-        x_lower, y_lower, v_lower = get_variable(data, variable,lower_layer,TS)
-        x_lower = np.ma.getdata(x_lower, False)
-        y_lower = np.ma.getdata(y_lower, False)
-        v_lower = np.ma.getdata(v_lower, False)
-       
-        x_plot_lower, v_plot_lower = x_y_interpolation(x_lower, y_lower, v_lower,lower_layer,TS, center_line) 
-        
-        
-        X_interpolated_centerline= []
-        V_interpolated_centerline= []
-                
-            
-        
-        for i in range(len(x_plot_upper)-1):
-            X = np.interp(depth, (lower_layer, upper_layer), (x_plot_lower[i], x_plot_upper[i+1])) #not nessicary 
-            V = np.interp(depth, (lower_layer, upper_layer), (v_plot_lower[i], v_plot_upper[i+1]))                    
-            X_interpolated_centerline.append(X)
-            V_interpolated_centerline.append(V)
-        
-        x_plot= X_interpolated_centerline
-        v_plot= V_interpolated_centerline
-        
-    plt.plot(x_plot,v_plot)
-    plt.title(f'Depth {depth}')
-    units= data.variables[variable].units
-    cname=data.variables[variable].long_name
-    plt.xlabel('x (m)')
-    plt.ylabel(f'{cname} [{units}]')
-    plt.show() 
-        
+exdir= dirname(abspath(__file__))
+datadir = normpath(join(exdir,relpath('data/river/d3d')))
+filename= 'turbineTest_map.nc'
+data = netCDF4.Dataset(join(datadir,filename))
+
+
+x =  np.linspace (0.1, 17.9, num=100)
+y = np.linspace (1.1, 4.9, num=100)
+z=  1#np.linspace (0.2, 1.8, num=100) 
     
-variables= [ 'ucx', 'turkin1']
-for var in variables:
-    plot_centerline(data,var,3.3, -1, 3) 
+#request= np.array([ [x, y, z] for x, y, z in zip(x_new, y_new, z_new)]) 
+#request_points= pd.DataFrame(request, columns=[ 'x', 'y', 'z'])
+#points=request_points
+   
+variables= ['ucx']# , 'turkin1"]
+#
+var_data_df=get_all_data_points(data, variables[0],time_step=-1)
+points = create_points(x, y, z)
+points['ucx']=interpolate_data(var_data_df[['x','y','z']], var_data_df[['ucx']],
+            points[['x','y','z']])  
+
+points.dropna(inplace=True)
+plt.tricontourf(points.x,points.y,points.ucx)
+#         units= data.variables[variable].units
+#         cname=data.variables[variable].long_name
+#         cbar.set_label(f'{cname} [{units}]')
+#         plt.xlabel('x (m)')# user input 
+#         plt.ylabel('y (m)')
+#         plt.show()
