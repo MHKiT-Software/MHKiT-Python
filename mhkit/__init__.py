c6d9abf14425709ee3584c65532a3b9d7cab1c77
rpauly18 <rpauly@nrel.gov>
2022-03-28 10:52:07 -0600

    updating version number in package

--- mhkit/__init__.py
+++ mhkit/__init__.py
@@ -15,7 +15,7 @@ _rmc()
 import warnings as _warn
 _warn.simplefilter(action='ignore', category=FutureWarning)
 
-__version__ = 'v0.4.0'
+__version__ = 'v0.5.0'
 
 __copyright__ = """
 Copyright 2019, Alliance for Sustainable Energy, LLC under the terms of 
