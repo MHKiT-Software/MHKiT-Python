ab4ce2373ad2e6db2322016cd28eb590da8ae96a
rpauly18 <rpauly@nrel.gov>
2022-03-31 12:48:57 -0600

    fixing dolfyn imports from pip

---
+++ mhkit/dolfyn/rotate/__init__.py
@@ -0,0 +1,6 @@
+from mhkit.dolfyn.rotate import api
+from mhkit.dolfyn.rotate import awac
+from mhkit.dolfyn.rotate import base
+from mhkit.dolfyn.rotate import rdi
+from mhkit.dolfyn.rotate import signature
+from mhkit.dolfyn.rotate import vector
