399b9e941e9d6d5b3c20428c9183b447af9a4d17
jmcvey3 <jmcvey3@users.noreply.github.com>
2022-05-10 09:18:25 -0700

    Readability, imports

--- mhkit/tests/dolfyn/test_motion.py
+++ mhkit/tests/dolfyn/test_motion.py
@@ -1,29 +1,30 @@
 from . import test_read_adv as tv
 #from . import test_read_adp as tp
 from .base import load_netcdf as load, save_netcdf as save, assert_allclose
-import dolfyn.adv.api as avm
+from mhkit.dolfyn.adv import api
 import unittest
 
 make_data = False
 
+
 class mc_testcase(unittest.TestCase):
     def test_motion_adv(self):
         tdm = tv.dat_imu.copy(deep=True)
-        tdm = avm.correct_motion(tdm)
+        tdm = api.correct_motion(tdm)
 
         # user added metadata
         tdmj = tv.dat_imu_json.copy(deep=True)
-        tdmj = avm.correct_motion(tdmj)
+        tdmj = api.correct_motion(tdmj)
 
         # set declination and then correct
         tdm10 = tv.dat_imu.copy(deep=True)
         tdm10.velds.set_declination(10.0, inplace=True)
-        tdm10 = avm.correct_motion(tdm10)
+        tdm10 = api.correct_motion(tdm10)
 
         # test setting declination to 0 doesn't affect correction
         tdm0 = tv.dat_imu.copy(deep=True)
         tdm0.velds.set_declination(0.0, inplace=True)
-        tdm0 = avm.correct_motion(tdm0)
+        tdm0 = api.correct_motion(tdm0)
         tdm0.attrs.pop('declination')
         tdm0.attrs.pop('declination_in_orientmat')
 
@@ -31,7 +32,7 @@ class mc_testcase(unittest.TestCase):
         tdmE = tv.dat_imu.copy(deep=True)
         tdmE.velds.set_declination(10.0, inplace=True)
         tdmE.velds.rotate2('earth', inplace=True)
-        tdmE = avm.correct_motion(tdmE)
+        tdmE = api.correct_motion(tdmE)
 
         if make_data:
             save(tdm, 'vector_data_imu01_mc.nc')
@@ -48,17 +49,15 @@ class mc_testcase(unittest.TestCase):
         assert_allclose(tdmE, cdm10, atol=1e-7)
         assert_allclose(tdmj, load('vector_data_imu01-json_mc.nc'), atol=1e-7)
 
-
     def test_sep_probes(self):
         tdm = tv.dat_imu.copy(deep=True)
-        tdm = avm.correct_motion(tdm, separate_probes=True)
+        tdm = api.correct_motion(tdm, separate_probes=True)
 
         if make_data:
             save(tdm, 'vector_data_imu01_mcsp.nc')
             return
 
         assert_allclose(tdm, load('vector_data_imu01_mcsp.nc'), atol=1e-7)
-
 
     # def test_motion_adcp(self):
     #     # Correction for ADCPs not completed yet
