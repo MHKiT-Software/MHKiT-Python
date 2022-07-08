363875907c47f6ab0eb79f8d1e4e97743cbaa76a
jmcvey3 <53623232+jmcvey3@users.noreply.github.com>
2022-07-08 13:02:27 -0700

    Add config details to dataset attributes

--- mhkit/tests/dolfyn/test_time.py
+++ mhkit/tests/dolfyn/test_time.py
@@ -17,10 +17,10 @@ class time_testcase(unittest.TestCase):
         dt_off = time.epoch2date(time.dt642epoch(td.time), offset_hr=-7)
         t_str = time.epoch2date(time.dt642epoch(td.time), to_str=True)
 
-        assert_equal(dt[0], datetime(2012, 6, 12, 12, 0, 2, 687282))
-        assert_equal(dt1, [datetime(2012, 6, 12, 12, 0, 2, 687282)])
-        assert_equal(dt_off[0], datetime(2012, 6, 12, 5, 0, 2, 687282))
-        assert_equal(t_str[0], '2012-06-12 12:00:02.687282')
+        assert_equal(dt[0], datetime(2012, 6, 12, 12, 0, 2, 687283))
+        assert_equal(dt1, [datetime(2012, 6, 12, 12, 0, 2, 687283)])
+        assert_equal(dt_off[0], datetime(2012, 6, 12, 5, 0, 2, 687283))
+        assert_equal(t_str[0], '2012-06-12 12:00:02.687283')
 
         # Validated based on data in ad2cp.index file
         assert_equal(time.dt642date(dat_sig.time[0])[0],
