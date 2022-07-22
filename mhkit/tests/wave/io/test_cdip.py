9c7eccedff948a732522820e30464765775d1731
ssolson <ssolson@users.noreply.github.com>
2022-07-22 09:49:41 -0600

    Test Suite Restructure (#174)
    * test_wave file to folder with individual feature files
    
    * Move io tests into io folder
    
    * Send plots to a plots folder
    
    * Move resource characterizations plot tests into resource metrics file
    
    * move load tests to folder
    
    * move power tests to folder
    
    * move river tests to folder
    
    * move tidal tests to folder
    
    * move utils tests to folder
    
    * add inits so that tests pick up the tests in subfolders
    
    * Require previous netCDF4 release.
    
    * FIx data directory path
    
    * Fix data directory path
    
    * Fix load data directory path
    
    * Remove relpath call to see if it fixes Windows test suite issues
    
    * Remove relpath from the rest of io tests in wave

---
+++ mhkit/tests/wave/io/test_cdip.py
@@ -0,0 +1,188 @@
+from os.path import abspath, dirname, join, isfile, normpath, relpath
+from pandas.testing import assert_frame_equal
+from numpy.testing import assert_allclose
+from scipy.interpolate import interp1d
+from random import seed, randint
+import matplotlib.pylab as plt
+from datetime import datetime
+import xarray.testing as xrt
+import mhkit.wave as wave
+from io import StringIO
+import pandas as pd
+import numpy as np
+import contextlib
+import unittest
+import netCDF4
+import inspect
+import pickle
+import time
+import json
+import sys
+import os
+
+
+testdir = dirname(abspath(__file__))
+datadir = normpath(join(testdir,'..','..','..','..','examples','data','wave'))
+
+
+class TestIOcdip(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        b067_1996='http://thredds.cdip.ucsd.edu/thredds/dodsC/cdip/' + \
+                   'archive/067p1/067p1_d04.nc'
+        self.test_nc = netCDF4.Dataset(b067_1996)
+
+        self.vars2D = [ 'waveEnergyDensity', 'waveMeanDirection',
+                        'waveA1Value', 'waveB1Value', 'waveA2Value',
+                        'waveB2Value', 'waveCheckFactor', 'waveSpread',
+                        'waveM2Value', 'waveN2Value']
+
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+    def test_validate_date(self):
+        date='2013-11-12'
+        start_date = wave.io.cdip._validate_date(date)
+        assert isinstance(start_date, datetime)
+
+        date='11-12-2012'
+        self.assertRaises(ValueError, wave.io.cdip._validate_date, date)
+
+    def test_request_netCDF_historic(self):
+        station_number='067'
+        nc = wave.io.cdip.request_netCDF(station_number, 'historic')
+        isinstance(nc, netCDF4.Dataset)
+
+    def test_request_netCDF_realtime(self):
+        station_number='067'
+        nc = wave.io.cdip.request_netCDF(station_number, 'realtime')
+        isinstance(nc, netCDF4.Dataset)
+
+
+    def test_start_and_end_of_year(self):
+        year = 2020
+        start_day, end_day = wave.io.cdip._start_and_end_of_year(year)
+
+        assert isinstance(start_day, datetime)
+        assert isinstance(end_day, datetime)
+
+        expected_start = datetime(year,1,1)
+        expected_end = datetime(year,12,31)
+
+        self.assertEqual(start_day, expected_start)
+        self.assertEqual(end_day, expected_end)
+
+    def test_dates_to_timestamp(self):
+
+        start_date='1996-10-02'
+        end_date='1996-10-20'
+
+        start_stamp, end_stamp = wave.io.cdip._dates_to_timestamp(self.test_nc,
+            start_date=start_date, end_date=end_date)
+
+        start_dt =  datetime.utcfromtimestamp(start_stamp)
+        end_dt =  datetime.utcfromtimestamp(end_stamp)
+
+        self.assertTrue(start_dt.strftime('%Y-%m-%d') == start_date)
+        self.assertTrue(end_dt.strftime('%Y-%m-%d') == end_date)
+
+    def test_get_netcdf_variables_all2Dvars(self):
+        data = wave.io.cdip.get_netcdf_variables(self.test_nc,
+            all_2D_variables=True)
+        returned_keys = [key for key in data['data']['wave2D'].keys()]
+        self.assertTrue( returned_keys == self.vars2D)
+
+    def test_get_netcdf_variables_params(self):
+        parameters =['waveHs', 'waveTp','notParam', 'waveMeanDirection']
+        data = wave.io.cdip.get_netcdf_variables(self.test_nc,
+            parameters=parameters)
+
+        returned_keys_1D = [key for key in data['data']['wave'].keys()]
+        returned_keys_2D = [key for key in data['data']['wave2D'].keys()]
+        returned_keys_metadata = [key for key in data['metadata']['wave']]
+
+        self.assertTrue( returned_keys_1D == ['waveHs', 'waveTp'])
+        self.assertTrue( returned_keys_2D == ['waveMeanDirection'])
+        self.assertTrue( returned_keys_metadata == ['waveFrequency'])
+
+
+    def test_get_netcdf_variables_time_slice(self):
+        start_date='1996-10-01'
+        end_date='1996-10-31'
+
+        data = wave.io.cdip.get_netcdf_variables(self.test_nc,
+                start_date=start_date, end_date=end_date,
+                parameters='waveHs')
+
+        start_dt = datetime.strptime(start_date, '%Y-%m-%d')
+        end_dt = datetime.strptime(end_date, '%Y-%m-%d')
+
+        self.assertTrue(data['data']['wave'].index[-1] < end_dt)
+        self.assertTrue(data['data']['wave'].index[0] > start_dt)
+
+
+    def test_request_parse_workflow_multiyear(self):
+        station_number = '067'
+        year1=2011
+        year2=2013
+        years = [year1, year2]
+        parameters =['waveHs', 'waveMeanDirection', 'waveA1Value']
+        data = wave.io.cdip.request_parse_workflow(station_number=station_number,
+            years=years, parameters =parameters )
+
+        expected_index0 = datetime(year1,1,1)
+        expected_index_final = datetime(year2,12,31)
+
+        wave1D = data['data']['wave']
+        self.assertEqual(wave1D.index[0].floor('d').to_pydatetime(), expected_index0)
+
+        self.assertEqual(wave1D.index[-1].floor('d').to_pydatetime(), expected_index_final)
+
+        for key,wave2D  in data['data']['wave2D'].items():
+            self.assertEqual(wave2D.index[0].floor('d').to_pydatetime(), expected_index0)
+            self.assertEqual(wave2D.index[-1].floor('d').to_pydatetime(), expected_index_final)
+
+
+    def test_plot_boxplot(self):
+        filename = abspath(join(testdir, 'wave_plot_boxplot.png'))
+        if isfile(filename):
+            os.remove(filename)
+
+        station_number = '067'
+        year = 2011
+        data = wave.io.cdip.request_parse_workflow(station_number=station_number,years=year,
+                       parameters =['waveHs'],
+                       all_2D_variables=False)
+
+        plt.figure()
+        wave.graphics.plot_boxplot(data['data']['wave']['waveHs'])
+        plt.savefig(filename, format='png')
+        plt.close()
+
+        self.assertTrue(isfile(filename))
+
+
+    def test_plot_compendium(self):
+        filename = abspath(join(testdir, 'wave_plot_boxplot.png'))
+        if isfile(filename):
+            os.remove(filename)
+
+        station_number = '067'
+        year = 2011
+        data = wave.io.cdip.request_parse_workflow(station_number=station_number,years=year,
+                       parameters =['waveHs', 'waveTp', 'waveDp'],
+                       all_2D_variables=False)
+
+        plt.figure()
+        wave.graphics.plot_compendium(data['data']['wave']['waveHs'],
+            data['data']['wave']['waveTp'], data['data']['wave']['waveDp'] )
+        plt.savefig(filename, format='png')
+        plt.close()
+
+        self.assertTrue(isfile(filename))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ mhkit/tests/wave/test_performance.py
@@ -0,0 +1,130 @@
+from os.path import abspath, dirname, join, isfile, normpath, relpath
+from pandas.testing import assert_frame_equal
+from numpy.testing import assert_allclose
+from scipy.interpolate import interp1d
+from random import seed, randint
+import matplotlib.pylab as plt
+from datetime import datetime
+import xarray.testing as xrt
+import mhkit.wave as wave
+from io import StringIO
+import pandas as pd
+import numpy as np
+import contextlib
+import unittest
+import netCDF4
+import inspect
+import pickle
+import time
+import json
+import sys
+import os
+
+
+testdir = dirname(abspath(__file__))
+plotdir = join(testdir, 'plots')
+isdir = os.path.isdir(plotdir)
+if not isdir: os.mkdir(plotdir)
+datadir = normpath(join(testdir,relpath('../../../examples/data/wave')))
+
+
+class TestPerformance(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        np.random.seed(123)
+        Hm0 = np.random.rayleigh(4, 100000)
+        Te = np.random.normal(4.5, .8, 100000)
+        P = np.random.normal(200, 40, 100000)
+        J = np.random.normal(300, 10, 100000)
+        ndbc_data_file = join(datadir,'data.txt')
+        [raw_ndbc_data, meta] = wave.io.ndbc.read_file(ndbc_data_file)
+        self.S = raw_ndbc_data.T
+
+        self.data = pd.DataFrame({'Hm0': Hm0, 'Te': Te, 'P': P,'J': J})
+        self.Hm0_bins = np.arange(0,19,0.5)
+        self.Te_bins = np.arange(0,9,1)
+        self.expected_stats = ["mean","std","median","count","sum","min","max","freq"]
+
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+    def test_capture_length(self):
+        L = wave.performance.capture_length(self.data['P'], self.data['J'])
+        L_stats = wave.performance.statistics(L)
+
+        self.assertAlmostEqual(L_stats['mean'], 0.6676, 3)
+
+    def test_capture_length_matrix(self):
+        L = wave.performance.capture_length(self.data['P'], self.data['J'])
+        LM = wave.performance.capture_length_matrix(self.data['Hm0'], self.data['Te'],
+                        L, 'std', self.Hm0_bins, self.Te_bins)
+
+        self.assertEqual(LM.shape, (38,9))
+        self.assertEqual(LM.isna().sum().sum(), 131)
+
+    def test_wave_energy_flux_matrix(self):
+        JM = wave.performance.wave_energy_flux_matrix(self.data['Hm0'], self.data['Te'],
+                        self.data['J'], 'mean', self.Hm0_bins, self.Te_bins)
+
+        self.assertEqual(JM.shape, (38,9))
+        self.assertEqual(JM.isna().sum().sum(), 131)
+
+    def test_power_matrix(self):
+        L = wave.performance.capture_length(self.data['P'], self.data['J'])
+        LM = wave.performance.capture_length_matrix(self.data['Hm0'], self.data['Te'],
+                        L, 'mean', self.Hm0_bins, self.Te_bins)
+        JM = wave.performance.wave_energy_flux_matrix(self.data['Hm0'], self.data['Te'],
+                        self.data['J'], 'mean', self.Hm0_bins, self.Te_bins)
+        PM = wave.performance.power_matrix(LM, JM)
+
+        self.assertEqual(PM.shape, (38,9))
+        self.assertEqual(PM.isna().sum().sum(), 131)
+
+    def test_mean_annual_energy_production(self):
+        L = wave.performance.capture_length(self.data['P'], self.data['J'])
+        maep = wave.performance.mean_annual_energy_production_timeseries(L, self.data['J'])
+
+        self.assertAlmostEqual(maep, 1754020.077, 2)
+
+
+    def test_plot_matrix(self):
+        filename = abspath(join(plotdir, 'wave_plot_matrix.png'))
+        if isfile(filename):
+            os.remove(filename)
+
+        M = wave.performance.wave_energy_flux_matrix(self.data['Hm0'], self.data['Te'],
+                        self.data['J'], 'mean', self.Hm0_bins, self.Te_bins)
+
+        plt.figure()
+        wave.graphics.plot_matrix(M)
+        plt.savefig(filename, format='png')
+        plt.close()
+
+        self.assertTrue(isfile(filename))
+
+    def test_powerperformance_workflow(self):
+        filename = abspath(join(plotdir, 'Capture Length Matrix mean.png'))
+        if isfile(filename):
+            os.remove(filename)
+        P = pd.Series(np.random.normal(200, 40, 743),index = self.S.columns)
+        statistic = ['mean']
+        savepath = plotdir
+        show_values = True
+        h = 60
+        expected = 401239.4822345051
+        x = self.S.T
+        CM,MAEP = wave.performance.power_performance_workflow(self.S, h,
+                        P, statistic, savepath=savepath, show_values=show_values)
+
+        self.assertTrue(isfile(filename))
+        self.assertEqual(list(CM.data_vars),self.expected_stats)
+
+        error = (expected-MAEP)/expected # SSE
+
+        self.assertLess(error, 1e-6)
+
+
+if __name__ == '__main__':
+    unittest.main()
