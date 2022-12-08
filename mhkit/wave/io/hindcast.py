5a674a109eeb09242234e999fd5f9511be74c5df
ssolson <ssolson@sandia.gov>
2022-12-08 10:47:26 -0700

    direction_ws request broken into multiple requests

--- mhkit/wave/io/hindcast.py
+++ mhkit/wave/io/hindcast.py
@@ -216,13 +216,53 @@ def request_wpto_directional_spectrum(lat_lon, year, tree=None,
     waveKwargs = {'tree':tree,'unscale':unscale,'str_decode':str_decode, 'hsds':hsds}
         
     with WaveX(wave_path, **waveKwargs) as rex_waves:
-        # get data
-        data_raw = rex_waves.get_lat_lon_df(parameter,lat_lon)
-        # get metadata
-        col = data_raw.columns[:]
-        meta = rex_waves.meta.loc[col,:]
-        meta = meta.reset_index(drop=True) 
+        # Get graphical identifier
+        gid = rex_waves.lat_lon_gid(lat_lon)  
+        
+        # Setup index and columns
+        if isinstance(gid, (int, np.integer)):
+            columns = [gid]
+        else:
+            columns = gid   
+        time_index = rex_waves.time_index
+        frequency = rex_waves['frequency']
+        direction = rex_waves['direction']
+        index = pd.MultiIndex.from_product(
+            [time_index, frequency, direction],
+            names=['time_index', 'frequency', 'direction']
+        )                    
 
-    data = data_raw.to_xarray()
+        # Create bins for multiple smaller API dataset requests
+        N=4
+        length = len(rex_waves)        
+        quotient=length//N
+        remainder=length%N    
+        bins = [i*quotient for i in range(N+1) ]
+        bins[-1]+= remainder        
+        index_bins = (np.array(bins)*len(frequency)*len(direction)).tolist()
+        
+        # Request multiple datasets and add to dictionary
+        datas={}
+        for i in range(len(bins)-1):
+            idx=index[index_bins[i]:index_bins[i+1]] 
+            data_array = rex_waves[parameter, bins[i]:bins[i+1], :, :, gid]
 
-    return data, meta    
+            ax1 = np.product(data_array.shape[:3])
+            ax2 = data_array.shape[-1] if len(data_array.shape) == 4 else 1
+            data_array = data_array.reshape(ax1, ax2)        
+            
+            df = pd.DataFrame(data_array, columns=columns, index=idx)
+            df.name = parameter
+            datas[i]=df
+
+        # Appednd each request into an xarray
+        data_raw=datas[0]
+        for i in list(datas.keys())[1:]:
+            data_raw =  pd.concat([data_raw,datas[i]])        
+        data = data_raw.to_xarray()
+
+        # Get metadata
+        meta = rex_waves.meta.loc[columns,:]
+        meta = meta.reset_index(drop=True)  
+
+    return data, meta
