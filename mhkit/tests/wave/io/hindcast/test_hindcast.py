1a51b8acea2fdeb137dd3f5641840587ccd111be
ssolson <ssolson@sandia.gov>
2023-04-05 09:11:04 -0600

    Add module level docstring

--- mhkit/tests/wave/io/hindcast/test_hindcast.py
+++ mhkit/tests/wave/io/hindcast/test_hindcast.py
@@ -1,3 +1,27 @@
+"""
+This module contains unit tests for the WPTO hindcast data retrieval 
+functions in the mhkit.wave package. The tests are designed to verify
+the correct functioning of the following functionalities:
+
+1. Retrieval of multiple years of data for a single data type,
+    latitude-longitude pair, and parameter.
+2. Retrieval of multiple parameters for a single data type, year, 
+    and latitude-longitude pair.
+3. Retrieval of data for multiple locations for point data and
+    directional spectrum at a single data type, year, and parameter.
+
+The tests use the unittest framework and compare the output of the 
+hindcast retrieval functions with expected output data. The expected 
+data is read from CSV files located in the examples/data/wave directory.
+
+Functions tested:
+- wave.io.hindcast.hindcast.request_wpto_point_data
+- wave.io.hindcast.hindcast.request_wpto_directional_spectrum
+
+Usage:
+Run the script directly as a standalone program, or import the 
+TestWPTOhindcast class in another test suite.
+"""
 import unittest
 from os.path import abspath, dirname, join, normpath
 from pandas.testing import assert_frame_equal
