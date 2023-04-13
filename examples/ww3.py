051c1e0539f99c2bd0531c0d62af4b6fb725a2bd
ssolson <ssolson@sandia.gov>
2023-04-13 09:40:30 -0600

    small changes

--- examples/ww3.py
+++ examples/ww3.py
@@ -1,18 +1,79 @@
-import mhkit.wave.io.ww3 as mio
-import datetime
+import time
+import os
 import xarray as xr
 import matplotlib.pyplot as plt
-# import cartopy.crs as ccrs
+# # import cartopy.crs as ccrs
+# import mhkit.wave.io.ww3 as mio
+# import datetime
 
 
-date = datetime.datetime(2011,1,1)
-fn = mio.request_data(date,23,['Tper'])
-# fn = '2011-01-01.nc'
-ds = xr.open_dataset(fn).squeeze().drop('depth')
+
+# from erddapClient import ERDDAP_Server
+
+# remote = ERDDAP_Server('https://coastwatch.pfeg.noaa.gov/erddap')
+# searchResults = remote.search(searchFor="WW3")
+# ww3 = searchResults[0]
+
+# print("Search results:", searchResults)
+# print(ww3)
+
+# import ipdb; ipdb.set_trace()
+
+path = './ww3.nc'
+
+if os.path.exists(path):
+    ds = xr.open_dataset('ww3.nc')
+else:
+    from erddapClient import ERDDAP_Griddap
+    start_time = time.perf_counter()
+
+    # remoteNDBC = ERDDAP_Server('https://coastwatch.pfeg.noaa.gov/erddap')
+    remote = ERDDAP_Griddap('https://coastwatch.pfeg.noaa.gov/erddap', 'NWW3_Global_Best')
+    # print("remoteNDBC:", remote)
+    # print(remote.dimensions)
+    # print(remote.dimensions['depth'].data)
+    # print("variables:", remote.variables.keys())
+    # print("summary:", remote.info['summary'])
+    # print("start:", remote.getAttribute('time_coverage_start'))
+
+
+    ds = (
+        remote.setResultVariables(['Tper']).setSubset(
+            time=slice('2011-01-01','2011-01-02'),
+            depth=0,
+            latitude=slice(-77.5,77.5),
+            longitude=slice(0, 359.5)
+        )
+        .getData(filetype='nc')
+    )
+
+
+    print("Writing to file" )
+    fb = open(path,'wb')
+    fb.write(ds)
+    fb.close()
+
+    end_time = time.perf_counter()
+
+    elapsed_time = end_time - start_time
+    print(f"Time taken: {elapsed_time:.4f} seconds")
+
+
 fig = plt.figure(figsize=(16/2,10/1.5/2))
-# ax = plt.axes(projection=ccrs.Orthographic(-80, 35))
-p = ds['Tper'].mean('time').plot.contourf(x='longitude',
-                                          y='latitude',)
-                                        #   ax=ax) 
-plt.title('Average wave period on {}'.format(date.strftime('%Y-%m-%d')))
+mean_Tper = ds['Tper'].mean('time')
+mean_Tper = mean_Tper.isel(depth=0)
+mean_Tper = mean_Tper.transpose('latitude', 'longitude')
+mean_Tper.plot.contourf(x='longitude', y='latitude')
 plt.show()
+
+# date = datetime.datetime(2011,1,1)
+# fn = mio.request_data(date, 23, ['Tper'])
+# # fn = '2011-01-01.nc'
+# ds = xr.open_dataset(fn).squeeze().drop('depth')
+# fig = plt.figure(figsize=(16/2,10/1.5/2))
+# # ax = plt.axes(projection=ccrs.Orthographic(-80, 35))
+# p = ds['Tper'].mean('time').plot.contourf(x='longitude',
+#                                           y='latitude',)
+#                                         #   ax=ax) 
+# plt.title('Average wave period on {}'.format(date.strftime('%Y-%m-%d')))
+# plt.show()
--- mhkit/wave/io/ww3.py
+++ mhkit/wave/io/ww3.py
@@ -4,6 +4,65 @@ import time
 import datetime
 
 
+def request_data(start_date, hours, varnames, latBounds=None, lonBounds=None, savepath=None):
+    """
+    Downloads data from global WW3 model and saves locally as a NetCDF-3 binary 
+    file with COARDS/CF/ACDD metadata.
+    https://pae-paha.pacioos.hawaii.edu/erddap/griddap/ww3_global.html
+    Individual file size limit is 2 GB
+    
+    Parameters
+    ------------
+    sdate: datetime.datetime
+        Start date
+    hours: float
+        Number of hours after start date (23 = 1 day)
+    latBounds: list
+        Latitude bounds, e.g., [0, 77.5] for northern hemisphere
+    lonBounds: list
+        Longitude bounds
+    varnames: list
+        Desired variable names (see https://pae-paha.pacioos.hawaii.edu/erddap/griddap/ww3_global.html)
+    savepath: string
+        File path for saving downloaded data
+    
+    Returns
+    ---------
+    ofn: string
+        Saved data file name
+    """
+    assert isinstance(start_date, datetime.datetime), 'start_date must be of type datetime.datetime'
+    assert isinstance(hours, (int, float)), 'hours must be of type float'
+    assert isinstance(varnames, list)
+
+
+    if latBounds is not None:
+        assert isinstance(latBounds, list), 'latBounds must be of type float or list'
+        assert len(latBounds) == 2, 'latBounds must be of length 2'
+        assert latBounds[1] > latBounds[0], 'latBounds[1] must be greater than latBounds[1]'
+    else:
+        latBounds = [-77.5, 77.5]
+    if lonBounds is not None:
+        assert isinstance(lonBounds, list), 'lonBounds must be of type float or list'
+        assert len(lonBounds) == 2, 'lonBounds must be of length 2'
+        assert lonBounds[1] > lonBounds[0], 'lonBounds[1] must be greater than lonBounds[1]'
+    else:
+        lonBounds = [0.0, 359.5]
+    if savepath is not None:
+        assert isinstance(savepath, str), 'savepath must be of type str'
+    else:
+        savepath = '.'
+
+    end_date = start_date + datetime.timedelta(hours=hours)
+
+    print('\n\n{date}'.format(date=start_date.strftime('%Y-%m-%d')))
+    url = _createUrl(start_date, end_date, latBounds, lonBounds, varnames)
+    outname = '{fn}.nc'.format(fn=start_date.strftime('%Y-%m-%d'))
+    ofn = os.path.join(savepath, outname)
+    _downloadFile(url, ofn)
+
+    return ofn
+
 def _createUrl(sdate, edate, latBounds, lonBounds, varnames):
     """
     Formulates URL for data from WW3 global model
@@ -73,25 +132,8 @@ def _createUrl(sdate, edate, latBounds, lonBounds, varnames):
 
     return outUrl
 
-def _reporthook(count, block_size, total_size):
-    """
-    Shows download progress
-    
-    Parameters
-    ------------
-    TODO
-    """
 
-    global start_time
-    if count == 0:
-        start_time = time.time()
-        return
-    duration = time.time() - start_time
-    progress_size = count * block_size / (1024 ** 2)
-    speed = progress_size / (duration)
-    percent = count * block_size * 100 / total_size
-    print('\t{size:.1f} MB, {speed:.3f} MB/s, {dur:.1f} s passed'.format(size=progress_size, 
-        speed=speed, dur=duration), end='\r', flush=True)
+
 
 def _downloadFile(url, outFileName):
     """
@@ -126,62 +168,25 @@ def _downloadFile(url, outFileName):
                 time.sleep(1)
     if attempts == 10:
         raise Exception("Error")
+    
 
-def request_data(start_date, hours, varnames, latBounds=None, lonBounds=None, savepath=None):
+def _reporthook(count, block_size, total_size):
     """
-    Downloads data from global WW3 model and saves locally as a NetCDF-3 binary 
-    file with COARDS/CF/ACDD metadata.
-    https://pae-paha.pacioos.hawaii.edu/erddap/griddap/ww3_global.html
-    Individual file size limit is 2 GB
+    Shows download progress
     
     Parameters
     ------------
-    sdate: datetime.datetime
-        Start date
-    hours: float
-        Number of hours after start date (23 = 1 day)
-    latBounds: list
-        Latitude bounds, e.g., [0, 77.5] for northern hemisphere
-    lonBounds: list
-        Longitude bounds
-    varnames: list
-        Desired variable names (see https://pae-paha.pacioos.hawaii.edu/erddap/griddap/ww3_global.html)
-    savepath: string
-        File path for saving downloaded data
-    
-    Returns
-    ---------
-    ofn: string
-        Saved data file name
+    TODO
     """
-    assert isinstance(start_date, datetime.datetime), 'start_date must be of type datetime.datetime'
-    assert isinstance(hours, (int, float)), 'hours must be of type float'
-    assert isinstance(varnames, list)
 
-
-    if latBounds is not None:
-        assert isinstance(latBounds, list), 'latBounds must be of type float or list'
-        assert len(latBounds) == 2, 'latBounds must be of length 2'
-        assert latBounds[1] > latBounds[0], 'latBounds[1] must be greater than latBounds[1]'
-    else:
-        latBounds = [-77.5, 77.5]
-    if lonBounds is not None:
-        assert isinstance(lonBounds, list), 'lonBounds must be of type float or list'
-        assert len(lonBounds) == 2, 'lonBounds must be of length 2'
-        assert lonBounds[1] > lonBounds[0], 'lonBounds[1] must be greater than lonBounds[1]'
-    else:
-        lonBounds = [0.0, 359.5]
-    if savepath is not None:
-        assert isinstance(savepath, str), 'savepath must be of type str'
-    else:
-        savepath = '.'
-
-    end_date = start_date + datetime.timedelta(hours=hours)
-
-    print('\n\n{date}'.format(date=start_date.strftime('%Y-%m-%d')))
-    url = _createUrl(start_date, end_date, latBounds, lonBounds, varnames)
-    outname = '{fn}.nc'.format(fn=start_date.strftime('%Y-%m-%d'))
-    ofn = os.path.join(savepath, outname)
-    _downloadFile(url, ofn)
-
-    return ofn
+    global start_time
+    if count == 0:
+        start_time = time.time()
+        return
+    duration = time.time() - start_time
+    progress_size = count * block_size / (1024 ** 2)
+    speed = progress_size / (duration)
+    percent = count * block_size * 100 / total_size
+    print('\t{size:.1f} MB, {speed:.3f} MB/s, {dur:.1f} s passed'.format(size=progress_size, 
+        speed=speed, dur=duration), end='\r', flush=True)
+    
