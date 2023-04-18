544ccfd4a06d1ff5ffe4dd5a5f13bf17b2f4db03
hivanov-nrel <53838851+hivanov-nrel@users.noreply.github.com>
2023-04-18 14:23:03 -0600

    moordyn input file feature

--- mhkit/tests/mooring/test_mooring.py
+++ mhkit/tests/mooring/test_mooring.py
@@ -1,7 +1,7 @@
 import unittest
 from os.path import abspath, dirname, isfile, join, normpath, relpath
 
-import pandas as pd
+import xarray as xr
 
 import mhkit.mooring as mooring
 
@@ -11,9 +11,10 @@ datadir = normpath(join(testdir,'..','..','..','examples','data','mooring'))
 class TestIOmooring(unittest.TestCase):
 
     def test_moordyn_out(self):
-        fpath = join(datadir, 'FAST.MD.out')
-        df = mooring.io.moordyn_out(fpath)
-        isinstance(df, pd.DataFrame)
+        fpath = join(datadir, 'Test.MD.out')
+        inputpath = join(datadir, "testInput.MD.dat")
+        ds = mooring.io.moordyn(fpath, input_file=inputpath)
+        isinstance(ds, xr.Dataset)
 
 if __name__ == '__main__':
     unittest.main()
