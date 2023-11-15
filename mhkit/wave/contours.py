5e0121cd464246e744b532538efc6152f8d6b5d2
ssolson <ssolson@sandia.gov>
2023-11-15 11:31:42 -0700

    check is samples min or max included in the contour half

--- mhkit/wave/contours.py
+++ mhkit/wave/contours.py
@@ -1756,15 +1756,26 @@ def samples_contour(t_samples, t_contour, hs_contour):
     amax = np.argmax(t_contour)
     aamin = np.min([amin, amax])
     aamax = np.max([amin, amax])
-    # finds points along the contour
+
+    # Seperate points along the contour in 2 halves (upper & lower half)
     w1 = hs_contour[aamin:aamax]
     w2 = np.concatenate((hs_contour[aamax:], hs_contour[:aamin]))
-    if (np.max(w1) > np.max(w2)):
-        x1 = t_contour[aamin:aamax]
-        y1 = hs_contour[aamin:aamax]
+
+    # Get samples min and max
+    t_min, t_max = np.min(t_samples), np.max(t_samples)
+
+    # Choose the half of the contour with the largest wave height
+    if np.max(w1) > np.max(w2):
+        include_aamax = t_max >= t_contour[aamax] or t_min <= t_contour[aamin]
+        x1 = t_contour[aamin:aamax + int(include_aamax)]
+        y1 = hs_contour[aamin:aamax + int(include_aamax)]
     else:
-        x1 = np.concatenate((t_contour[aamax:], t_contour[:aamin]))
-        y1 = np.concatenate((hs_contour[aamax:], hs_contour[:aamin]))
+        include_aamin = t_max >= t_contour[aamin] or t_min <= t_contour[aamin]
+        x1 = np.concatenate(
+            (t_contour[aamax:], t_contour[:aamin + int(include_aamin)]))
+        y1 = np.concatenate(
+            (hs_contour[aamax:], hs_contour[:aamin + int(include_aamin)]))
+
     # sorts data based on the max and min energy period values
     ms = np.argsort(x1)
     x = x1[ms]
