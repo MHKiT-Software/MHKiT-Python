66e0386dd17c40a48f3f68e42e94f3c672410c06
akeeste <akeeste@sandia.gov>
2023-12-18 14:27:48 -0600

    finish converting power.quality to xarray

--- mhkit/power/characteristics.py
+++ mhkit/power/characteristics.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.signal import hilbert
 import datetime
 
-def instantaneous_frequency(um, to_pandas=True):
+def instantaneous_frequency(um, dimension="", to_pandas=True):
 
     """
     Calculates instantaneous frequency of measured voltage
@@ -13,6 +13,10 @@ def instantaneous_frequency(um, to_pandas=True):
     -----------
     um: pandas Series, pandas DataFrame, xarray DataArray, or xarray Dataset
         Measured voltage (V) indexed by time 
+
+    dimension: string (optional)
+        Name of the xarray dimension corresponding to time.
+        If not supplied, time is assumed to be the first dimension.
         
     to_pandas: bool (Optional)
         Flag to save output to pandas instead of xarray. Default = True.
@@ -26,31 +30,39 @@ def instantaneous_frequency(um, to_pandas=True):
     if not isinstance(um, (pd.Series, pd.DataFrame, xr.DataArray, xr.Dataset)):
         raise TypeError('um must be of type pd.Series, pd.DataFrame, ' + 
                         f'xr.DataArray, or xr.Dataset. Got{type(um)}')
-   
-    if isinstance(um.index[0], datetime.datetime):
+
+    # Convert data into xr.Dataset
+    if isinstance(um, (pd.DataFrame, pd.Series)):
+        um = um.to_xarray()
+    if um.name is None:
+        um.name = 'data'
+    if isinstance(um, xr.DataArray):
+        um = um.to_dataset()
+
+    # Get the dimension of interest
+    if dimension == "":
+        dimension = list(um.coords)[0]
+
+    # Calculate time step
+    if isinstance(um.coords[dimension].values[0], datetime.datetime):
         t = (um.index - datetime.datetime(1970,1,1)).total_seconds()
     else:
         t = um.index
-        
-    if isinstance(um, (pd.DataFrame, pd.Series)):
-        um = um.to_xarray()
+    dt = np.diff(t)
 
-    dt = pd.Series(t).diff()[1:]
-
-    if isinstance(um,pd.Series):
-        um = um.to_frame()
-
-    columns  = um.columns
-    frequency=pd.DataFrame(columns=columns)
-    for column in um.columns:
-        f = hilbert(um[column])
+    # Calculate frequency
+    frequency = xr.Dataset()
+    for var in um.data_vars:
+        f = hilbert(um[var])
         instantaneous_phase = np.unwrap(np.angle(f))
         instantaneous_frequency = np.diff(instantaneous_phase) /(2.0*np.pi) * (1/dt)
-        frequency[column] = instantaneous_frequency
-        
+
+        frequency = frequency.assign({var: (dimension, instantaneous_frequency)})
+        frequency = frequency.assign_coords({dimension: um.coords[dimension].values[0:-1]})
+
     if to_pandas:
         frequency = frequency.to_pandas()
-        
+
     return frequency
 
 def dc_power(voltage, current, to_pandas=True):
@@ -79,21 +91,37 @@ def dc_power(voltage, current, to_pandas=True):
     if not isinstance(current, (pd.Series, pd.DataFrame, xr.DataArray, xr.Dataset)):
         raise TypeError('current must be of type pd.Series, pd.DataFrame, ' + 
                         f'xr.DataArray, or xr.Dataset. Got{type(current)}')
-    if not voltage.shape == current.shape:
-        raise ValueError('current and volatge must have the same shape')
-    
+
+    # Convert input to xarray.Dataset
     if isinstance(voltage, (pd.DataFrame, pd.Series)):
         voltage = voltage.to_xarray()
-    
     if isinstance(current, (pd.DataFrame, pd.Series)):
         current = current.to_xarray()
+    if isinstance(voltage, xr.DataArray):
+        voltage = voltage.to_dataset()
+    if isinstance(current, xr.DataArray):
+        current = current.to_dataset()
+
+    # Check that sizes are the same
+    if not (voltage.sizes == current.sizes and len(voltage.data_vars) == len(current.data_vars)):
+        raise ValueError('current and voltage must have the same shape')
+
+    P = xr.Dataset()
+    gross = None
     
-    P = current.values * voltage.values
-    P = pd.DataFrame(P)
-    P['Gross'] = P.sum(axis=1, skipna=True)
-    
+    # Multiply current and voltage variables together, in order they're assigned
+    for i, (current_var, voltage_var) in enumerate(zip(current.data_vars,voltage.data_vars)):
+        temp = current[current_var]*voltage[voltage_var]
+        P = P.assign({f'{i}': temp})
+        if gross is None:
+            gross = temp
+        else:
+            gross = gross + temp
+
+    P = P.assign({'Gross': gross})
+
     if to_pandas:
-        P = P.to_pandas()
+        P = P.to_dataframe()
 
     return P
 
@@ -103,11 +131,11 @@ def ac_power_three_phase(voltage, current, power_factor, line_to_line=False, to_
 
     Parameters
     -----------
-    voltage: pandas DataFrame or xarray Dataset
-        Time-series of three phase measured voltage [V] indexed by time
+    voltage: pandas Series, pandas DataFrame, xarray DataArray, or xarray Dataset
+        Measured DC voltage [V] indexed by time
         
-    current: pandas DataFrame or xarray Dataset
-        Time-series of three phase measured current [A] indexed by time
+    current: pandas Series, pandas DataFrame, xarray DataArray, or xarray Dataset
+        Measured three phase current [A] indexed by time
         
     power_factor: float 
         Power factor for the efficiency of the system
@@ -123,38 +151,40 @@ def ac_power_three_phase(voltage, current, power_factor, line_to_line=False, to_
     P: pandas DataFrame or xarray Dataset
         Magnitude of active AC power [W] indexed by time with Power column 
     """
-    if not isinstance(voltage, (pd.DataFrame, xr.Dataset)):
-        raise TypeError('current must be of type pd.DataFrame ' + 
-                        f'or xr.Dataset. Got{type(current)}')
-    if not isinstance(current, (pd.DataFrame, xr.Dataset)):
-        raise TypeError('current must be of type pd.DataFrame ' + 
-                        f'or xr.Dataset. Got{type(current)}')
-    if not len(voltage.columns) == 3:
-        raise ValueError('voltage must have three columns')
-    if not len(current.columns) == 3:
-        raise ValueError('current must have three columns')
-    if not current.shape == voltage.shape:
-        raise ValueError('current and voltage must be of the same size')
-    
+    if not isinstance(voltage, (pd.Series, pd.DataFrame, xr.DataArray, xr.Dataset)):
+        raise TypeError('voltage must be of type pd.Series, pd.DataFrame, ' + 
+                        f'xr.DataArray, or xr.Dataset. Got{type(voltage)}')
+    if not isinstance(current, (pd.Series, pd.DataFrame, xr.DataArray, xr.Dataset)):
+        raise TypeError('current must be of type pd.Series, pd.DataFrame, ' + 
+                        f'xr.DataArray, or xr.Dataset. Got{type(current)}')
+
+    # Convert input to xarray.Dataset
     if isinstance(voltage, (pd.DataFrame, pd.Series)):
         voltage = voltage.to_xarray()
-    
     if isinstance(current, (pd.DataFrame, pd.Series)):
         current = current.to_xarray()
-    
-    abs_current = np.abs(current.values)
-    abs_voltage = np.abs(voltage.values)
+    if isinstance(voltage, xr.DataArray):
+        voltage = voltage.to_dataset()
+    if isinstance(current, xr.DataArray):
+        current = current.to_dataset()
+
+    # Check that sizes are the same
+    if not len(voltage.data_vars) == 3:
+        raise ValueError('voltage must have three columns')
+    if not len(current.data_vars) == 3:
+        raise ValueError('current must have three columns')
+    if not current.sizes == voltage.sizes:
+        raise ValueError('current and voltage must be of the same size')
+
+    power = dc_power(voltage, current, to_pandas=False)['Gross']
+    power.name = 'Power'
+    power = power.to_dataset() # force xr.DataArray to be consistently in xr.Dataset format
+    P = np.abs(power) * power_factor
 
     if line_to_line:
-        power = abs_current * (abs_voltage * np.sqrt(3))
-    else:
-        power = abs_current * abs_voltage
-         
-    power = pd.DataFrame(power) 
-    P = power.sum(axis=1) * power_factor
-    P = P.to_frame('Power')
-    
+        P = P * np.sqrt(3)
+
     if to_pandas:
         P = P.to_pandas()
-    
+
     return P
--- mhkit/tests/power/test_power.py
+++ mhkit/tests/power/test_power.py
@@ -52,8 +52,8 @@ class TestDevice(unittest.TestCase):
             
     def test_harmonics_sine_wave_xarray(self):
         current = xr.DataArray(data=self.signal, 
-                               dims='index',
-                               coords={'index':self.samples})
+                                dims='index',
+                                coords={'index':self.samples})
         harmonics = power.quality.harmonics(current, 1000, self.frequency)
         
         for i, j in zip(harmonics.values, self.harmonics_vals):
@@ -61,7 +61,7 @@ class TestDevice(unittest.TestCase):
 
     def test_harmonic_subgroup_sine_wave(self):
         harmonics = pd.DataFrame(self.harmonics_vals,
-                                 index=self.harmonics_int)
+                                  index=self.harmonics_int)
         hsg = power.quality.harmonic_subgroups(harmonics, self.frequency)
         
         for i, j in zip(hsg.values, self.harmonic_groups):
@@ -69,7 +69,7 @@ class TestDevice(unittest.TestCase):
 
     def test_harmonic_subgroup_sine_wave_xarray(self):
         harmonics = xr.Dataset(data_vars={'harmonics':(['index'], self.harmonics_vals)},
-                               coords={'index':self.harmonics_int})
+                                coords={'index':self.harmonics_int})
         hsg = power.quality.harmonic_subgroups(harmonics, self.frequency)
         
         for i, j in zip(hsg.values, self.harmonic_groups):
@@ -77,7 +77,7 @@ class TestDevice(unittest.TestCase):
 
     def test_TCHD_sine_wave(self):
         harmonics = pd.DataFrame(self.harmonics_vals,
-                                 index=self.harmonics_int)
+                                  index=self.harmonics_int)
         hsg = power.quality.harmonic_subgroups(harmonics, self.frequency)
         TCHD = power.quality.total_harmonic_current_distortion(
             hsg, 18.8)  # had to just put a random rated current in here
@@ -86,7 +86,7 @@ class TestDevice(unittest.TestCase):
 
     def test_TCHD_sine_wave_xarray(self):
         harmonics = xr.Dataset(data_vars={'harmonics':(['index'],self.harmonics_vals)},
-                               coords={'index':self.harmonics_int})
+                                coords={'index':self.harmonics_int})
         hsg = power.quality.harmonic_subgroups(harmonics, self.frequency)
         TCHD = power.quality.total_harmonic_current_distortion(
             hsg, 18.8)  # had to just put a random rated current in here
@@ -95,7 +95,7 @@ class TestDevice(unittest.TestCase):
 
     def test_interharmonics_sine_wave(self):
         harmonics = pd.DataFrame(self.harmonics_vals,
-                                 index=self.harmonics_int)
+                                  index=self.harmonics_int)
         inter_harmonics = power.quality.interharmonics(
             harmonics, self.frequency)
 
@@ -104,50 +104,75 @@ class TestDevice(unittest.TestCase):
             
     def test_interharmonics_sine_wave_xarray(self):
         harmonics = xr.Dataset(data_vars={'harmonics':(['index'],self.harmonics_vals)},
-                               coords={'index':self.harmonics_int})
+                                coords={'index':self.harmonics_int})
         inter_harmonics = power.quality.interharmonics(
             harmonics, self.frequency)
 
         for i, j in zip(inter_harmonics.values, self.interharmonic):
             self.assertAlmostEqual(i[0], j, 1)
 
-    # def test_instfreq(self):
-    #     um = pd.Series(self.signal, index=self.samples)
+    def test_instfreq(self):
+        um = pd.Series(self.signal, index=self.samples)
 
-    #     freq = power.characteristics.instantaneous_frequency(um)
-    #     for i in freq.values:
-    #         self.assertAlmostEqual(i[0], self.frequency, 1)
+        freq = power.characteristics.instantaneous_frequency(um)
+        for i in freq.values:
+            self.assertAlmostEqual(i[0], self.frequency, 1)
 
-    # def test_dc_power_DataFrame(self):
-    #     current = pd.DataFrame(self.current_data, columns=['A1', 'A2', 'A3'])
-    #     voltage = pd.DataFrame(self.voltage_data, columns=['V1', 'V2', 'V3'])
-    #     P = power.characteristics.dc_power(voltage, current)
-    #     self.assertEqual(P.sum()['Gross'],
-    #                       (voltage.values * current.values).sum())
+    def test_dc_power_pandas(self):
+        current = pd.DataFrame(self.current_data, columns=['A1','A2','A3'])
+        voltage = pd.DataFrame(self.voltage_data, columns=['V1','V2','V3'])
+        P = power.characteristics.dc_power(voltage, current)
+        self.assertEqual(P.sum()['Gross'], 584)
+        P = power.characteristics.dc_power(voltage['V1'], current['A1'])
+        self.assertEqual(P.sum()['Gross'], 70)
 
-    # def test_dc_power_Series(self):
-    #     current = pd.DataFrame(self.current_data, columns=['A1', 'A2', 'A3'])
-    #     voltage = pd.DataFrame(self.voltage_data, columns=['V1', 'V2', 'V3'])
-    #     P = power.characteristics.dc_power(voltage['V1'], current['A1'])
-    #     self.assertEqual(P.sum()['Gross'], sum(voltage['V1'] * current['A1']))
+    def test_dc_power_xarray(self):
+        current = pd.DataFrame(self.current_data, columns=['A1','A2','A3'])
+        voltage = pd.DataFrame(self.voltage_data, columns=['V1','V2','V3'])
+        current = current.to_xarray()
+        voltage = voltage.to_xarray()
+        P = power.characteristics.dc_power(voltage, current)
+        self.assertEqual(P.sum()['Gross'], 584)
+        P = power.characteristics.dc_power(voltage['V1'], current['A1'])
+        self.assertEqual(P.sum()['Gross'], 70)
 
-    # def test_ac_power_three_phase(self):
-    #     current = pd.DataFrame(self.current_data, columns=['A1', 'A2', 'A3'])
-    #     voltage = pd.DataFrame(self.voltage_data, columns=['V1', 'V2', 'V3'])
+    def test_ac_power_three_phase_pandas(self):
+        current = pd.DataFrame(self.current_data, columns=['A1','A2','A3'])
+        voltage = pd.DataFrame(self.voltage_data, columns=['V1','V2','V3'])
 
-    #     P1 = power.characteristics.ac_power_three_phase(
-    #         voltage, current, 1, False)
-    #     P1b = power.characteristics.ac_power_three_phase(
-    #         voltage, current, 0.5, False)
-    #     P2 = power.characteristics.ac_power_three_phase(
-    #         voltage, current, 1, True)
-    #     P2b = power.characteristics.ac_power_three_phase(
-    #         voltage, current, 0.5, True)
+        P1 = power.characteristics.ac_power_three_phase(
+            voltage, current, 1, False)
+        P1b = power.characteristics.ac_power_three_phase(
+            voltage, current, 0.5, False)
+        P2 = power.characteristics.ac_power_three_phase(
+            voltage, current, 1, True)
+        P2b = power.characteristics.ac_power_three_phase(
+            voltage, current, 0.5, True)
 
-    #     self.assertEqual(P1.sum().iloc[0], 584)
-    #     self.assertEqual(P1b.sum().iloc[0], 584/2)
-    #     self.assertAlmostEqual(P2.sum().iloc[0], 1011.518, 2)
-    #     self.assertAlmostEqual(P2b.sum().iloc[0], 1011.518/2, 2)
+        self.assertEqual(P1.sum().iloc[0], 584)
+        self.assertEqual(P1b.sum().iloc[0], 584/2)
+        self.assertAlmostEqual(P2.sum().iloc[0], 1011.518, 2)
+        self.assertAlmostEqual(P2b.sum().iloc[0], 1011.518/2, 2)
+
+    def test_ac_power_three_phase_xarray(self):
+        current = pd.DataFrame(self.current_data, columns=['A1','A2','A3'])
+        voltage = pd.DataFrame(self.voltage_data, columns=['V1','V2','V3'])
+        current = current.to_xarray()
+        voltage = voltage.to_xarray()
+
+        P1 = power.characteristics.ac_power_three_phase(
+            voltage, current, 1, False)
+        P1b = power.characteristics.ac_power_three_phase(
+            voltage, current, 0.5, False)
+        P2 = power.characteristics.ac_power_three_phase(
+            voltage, current, 1, True)
+        P2b = power.characteristics.ac_power_three_phase(
+            voltage, current, 0.5, True)
+
+        self.assertEqual(P1.sum().iloc[0], 584)
+        self.assertEqual(P1b.sum().iloc[0], 584/2)
+        self.assertAlmostEqual(P2.sum().iloc[0], 1011.518, 2)
+        self.assertAlmostEqual(P2b.sum().iloc[0], 1011.518/2, 2)
 
 
 if __name__ == '__main__':
