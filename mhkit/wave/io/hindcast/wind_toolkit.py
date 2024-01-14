954edfbc7cf7d1001d1d214428c2d5b6eee19ca6
ssolson <sterlingolson@yahoo.com>
2024-01-14 08:28:04 -0700

    black

--- mhkit/wave/io/hindcast/wind_toolkit.py
+++ mhkit/wave/io/hindcast/wind_toolkit.py
@@ -446,7 +446,6 @@ def request_wtk_point_data(
         }
         data_list = []
         with MultiYearWindX(wind_path, **windKwargs) as rex_wind:
-
             if isinstance(parameter, list):
                 for p in parameter:
                     temp_data = rex_wind.get_lat_lon_df(p, lat_lon)
