c648c248c3d00cd59063364db43372d472a59df4
jmcvey3 <53623232+jmcvey3@users.noreply.github.com>
2024-06-03 21:27:33 -0700

    Spectral analysis code

--- mhkit/acoustics/main.py
+++ mhkit/acoustics/main.py
@@ -1,49 +1,14 @@
 import numpy as np
 import xarray as xr
-from scipy.signal import periodogram, welch
+
+from mhkit.dolfyn import VelBinner
 
 
-def _fast_fourier_transform(fs, x):
-    # demean the time series
-    x -= np.mean(x)
-    # add hanning window
-    x *= np.hanning(len(x))
-    # Nyquist frequency
-    fn = fs / 2
-    # Next highest power of 2 greater than length(x).
-    n_fft = 2 * 2 ** (np.ceil(np.log(len(x)) / np.log(2)))
-    # Take FFT, padding with zeros
-    temp_fft = np.fft.fft(x, n_fft)
-    # FFT is symmetric, throw away second half
-    n_unique = np.ceil((n_fft + 1) / 2)
-    spec = temp_fft[:n_unique]
-    # Multiply by 2 to take into account the fact that we
-    # threw out second half of TempFFT above
-    spec = spec * 2
-    # Account for endpoint uniqueness
-    spec[0] = spec(1) / 2
-    # We know n_fft is even
-    spec[-1] = spec(len(spec)) / 2
-    # Scale the FFT so that it is not a function of the length of x.
-    spec = spec / len(x)
-    # Frequency vector
-    f = np.arange(n_unique) * 2 * fn / n_fft
-
-    return f, spec
-
-
-# Vo2 = 1  # reference v0^2, V^2/Hz
-# Po2 = 1e-12  # reference p0^2, Pa^2/Hz
-# G = 0  # amplifier gain in dB
-# fmin = 20  # frequency band lower limit of the hydrophone
-# fmax = 192000 / 2  # Nyquist frequency
-
-
-def sound_pressure_spectral_density(P, fs, window_size=1, overlap=0.5):
+def sound_pressure_spectral_density(P, fs, window_size=1):
     """
     Calculates the mean square sound pressure spectral density from audio
     samples split into FFTs with a specified window_size in seconds and
-    a specified percent overlap.
+    at least a 50% overlap.
 
     Parameters
     ----------
@@ -53,8 +18,6 @@ def sound_pressure_spectral_density(P, fs, window_size=1, overlap=0.5):
         Data collection sampling rate [Hz]
     window_size: string (optional)
         Length of time in seconds to create FFTs. Default is 1 s.
-    overlap: numeric
-        Percent overlap between neighboring FFTs. Default is 50%.
 
     Returns
     -------
@@ -64,47 +27,30 @@ def sound_pressure_spectral_density(P, fs, window_size=1, overlap=0.5):
     """
     # window length of each time series
     win = window_size * fs
-    # overlap between each window
-    step = int(overlap * fs)
-    # number of time series samples
-    ns = int(np.floor((len(P) - win) / step))
-    # number of fft points
-    nfft = int(2 * 2.0 ** (np.ceil(np.log(win) / np.log(2))))
-    # frequency resolution
-    df = fs / nfft
-    # Next highest power of 2 greater than length(x).
-    nfreq = int(np.ceil((nfft + 1) / 2))
-    # mean-squared sound pressure spectral density
-    Pf2 = np.zeros((nfreq, ns))
 
-    # Takes too long
-    for i in range(ns):
-        sample = P[i * step : i * step + win]
-        sample = sample - np.mean(sample)
-        # mean squred sound pressure; power in time domain
-        t_power = sum(sample * 2) / len(sample)
-        freq, spec = _fast_fourier_transform(fs, sample)
-        # PSD
-        psd = spec * np.conj(spec) / df / 2
-        # power in frequency domain
-        f_power = np.sum(psd) * df
-        # adjust the amplitude of PSD according to Parseval's theorem
-        psd_adj = psd * t_power / f_power
-        # mean-squared sound pressure spectral density
-        Pf2[:, i] = psd_adj
+    # Use dolfyn PSD
+    binner = VelBinner(n_bin=win, fs=fs, n_fft=win)
+    # Always 50% overlap if numbers reshape perfectly
+    # Mean square sound pressure
+    psd = binner.power_spectral_density(P, freq_units="Hz")
+    samples = binner.reshape(P.values) - binner.mean(P.values)[:, None]
+    # Power in time domain
+    t_power = np.sum(samples**2, axis=1) / win
+    # Power in frequency domain
+    f_power = psd.sum("freq") * (fs / win)
+    # Adjust the amplitude of PSD according to Parseval's theorem
+    psd_adj = psd * t_power[:, None] / f_power
 
-    ts = np.arange(len(P)) / fs  # time label for the whole time series
     out = xr.DataArray(
-        psd_adj.T,
-        coords={"time": ts, "freq": freq},
+        psd_adj,
+        coords={"time": psd_adj["time"], "freq": psd_adj["freq"]},
         attrs={
             "units": "Pa^2/Hz",
             "long_name": "Mean Square Sound Pressure Spectral Density",
             "fs": fs,
-            "window": win,
-            "overlap": overlap,
-            "n_samples": ns,
-            "nfft": nfft,
+            "window": str(window_size) + " s",
+            "overlap": "50%",
+            "nfft": win,
         },
     )
 
@@ -171,10 +117,11 @@ def sound_pressure_level(spsd, fmin=20, fmax=192000 // 2):
     nfmin = fmin // df
     nfmax = fmax // df
 
-    # Sound pressure level in a specified frequency band from mean square values
+    # Mean square sound pressure in a specified frequency band from mean square values
     P2 = (
         spsd.sel(freq=slice(nfmin, nfmax)).sum("freq").values * df
-    )  # mean square sound pressure
+    )
+    # Mean square sound pressure level
     mspl = 10 * np.log10(P2 / P2_ref)
 
     out = xr.DataArray(
--- test.py
+++ test.py
@@ -2,7 +2,11 @@ from mhkit import acoustics
 
 P = acoustics.io.read_iclisten("examples/data/acoustics/RBW_6661_20240601_053114.wav")
 
-# spsd = acoustics.sound_pressure_spectral_density(P, P.fs)
+spsd = acoustics.sound_pressure_spectral_density(P, P.fs)
+
+spsdl = acoustics.sound_pressure_spectral_density_level(spsd)
+
+spl = acoustics.sound_pressure_level(spsd, fmin=20, fmax=512000 // 2)
 
 # spsd.plot()
 print("here")
