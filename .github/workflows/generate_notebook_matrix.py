24623f7cf3aad214dc5bcb1c86b5d40509fa051d
ssolson <sterlingolson@yahoo.com>
2024-07-03 10:54:29 -0600

    remove condition from generated matrix

--- .github/workflows/generate_notebook_matrix.py
+++ .github/workflows/generate_notebook_matrix.py
@@ -1,5 +1,6 @@
 """
-Dynamically creates a matrix for the GitHub Actions workflow that runs the notebooks in the examples directory.
+Dynamically creates a matrix for the GitHub Actions workflow that
+runs the notebooks in the examples directory.
 """
 
 import os
@@ -14,12 +15,7 @@ for root, dirs, files in os.walk("examples"):
 # Generate the matrix configuration
 matrix = {"include": []}
 for notebook in notebooks:
-    if notebook.endswith("metocean_example.ipynb") or notebook.endswith(
-        "WPTO_hindcast_example.ipynb"
-    ):
-        matrix["include"].append({"notebook": notebook, "condition": True})
-    else:
-        matrix["include"].append({"notebook": notebook, "condition": False})
+    matrix["include"].append({"notebook": notebook})
 
 # Print the matrix as a properly formatted JSON string
 matrix_json = json.dumps(matrix)
