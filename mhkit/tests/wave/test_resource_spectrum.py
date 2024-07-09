cc7236654d7d98bd2059d256768578441d12950e
Andrew Simms <simms.andrew@gmail.com>
2024-07-09 08:43:33 -0600

    Test: Determine method using input frequency index

--- mhkit/tests/wave/test_resource_spectrum.py
+++ mhkit/tests/wave/test_resource_spectrum.py
@@ -158,6 +158,25 @@ class TestResourceSpectrum(unittest.TestCase):
 
         assert_allclose(eta_ifft, eta_sos)
 
+    def test_surface_elevation_uses_sum_of_sines_when_input_frequency_index_does_not_have_zero(self):
+        f = np.linspace(1 / 30, 1 / 2, 32)
+        S = wave.resource.jonswap_spectrum(f, self.Tp, self.Hs)
+
+        eta_default = wave.resource.surface_elevation(S, self.t, seed=1)
+        eta_sos = wave.resource.surface_elevation(
+            S, self.t, seed=1, method="sum_of_sines"
+        )
+
+        assert_allclose(eta_default, eta_sos)
+
+    def test_surface_elevation_uses_ifft_when_input_frequency_index_has_zero(self):
+        S = wave.resource.jonswap_spectrum(self.f, self.Tp, self.Hs)
+
+        eta_default = wave.resource.surface_elevation(S, self.t, seed=1)
+        eta_ifft = wave.resource.surface_elevation(S, self.t, seed=1, method="ifft")
+
+        assert_allclose(eta_default, eta_ifft)
+
     def test_plot_spectrum(self):
         filename = abspath(join(plotdir, "wave_plot_spectrum.png"))
         if isfile(filename):
