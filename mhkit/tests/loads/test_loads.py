3a170ff7c37d713798d56671cd4bd44c62764c6e
akeeste <akeeste@sandia.gov>
2024-10-02 15:29:58 -0500

    update type references in loads

--- mhkit/tests/loads/test_loads.py
+++ mhkit/tests/loads/test_loads.py
@@ -422,7 +422,7 @@ class TestWDRT(unittest.TestCase):
         mler["WaveSpectrum"] = self.mler["Res_Spec"].values
         mler["Phase"] = self.mler["phase"].values
         k = resource.wave_number(wave_freq, 70)
-        k = k.fillna(0)
+        np.nan_to_num(k, nan=0)
         mler_norm = loads.extreme.mler_wave_amp_normalize(
             4.5 * 1.9, mler, self.sim, k.k.values
         )
@@ -442,7 +442,7 @@ class TestWDRT(unittest.TestCase):
         mler["WaveSpectrum"] = self.mler["Norm_Spec"].values
         mler["Phase"] = self.mler["phase"].values
         k = resource.wave_number(wave_freq, 70)
-        k = k.fillna(0)
+        np.nan_to_num(k, nan=0)
         RAO = self.mler["RAO"].astype(complex)
         mler_ts = loads.extreme.mler_export_time_series(
             RAO.values, mler, self.sim, k.k.values
