ef669bd94fedd82777189f9fb477a00723b0e60e
akeeste <akeeste@sandia.gov>
2024-10-21 09:38:18 -0500

    use np.nan_to_num correctly in test_loads

--- mhkit/tests/loads/test_loads.py
+++ mhkit/tests/loads/test_loads.py
@@ -422,7 +422,7 @@ class TestWDRT(unittest.TestCase):
         mler["WaveSpectrum"] = self.mler["Res_Spec"].values
         mler["Phase"] = self.mler["phase"].values
         k = resource.wave_number(wave_freq, 70)
-        np.nan_to_num(k, 0)
+        k = np.nan_to_num(k, nan=0)
         mler_norm = loads.extreme.mler_wave_amp_normalize(4.5 * 1.9, mler, self.sim, k)
         mler_norm.reset_index(drop=True, inplace=True)
 
