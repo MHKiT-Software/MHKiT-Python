c5943301b3f4fe87b6c095a1834997b59cf01eeb
akeeste <akeeste@sandia.gov>
2024-11-21 15:02:57 -0600

    combine acoustics doc text with module-level docstring

--- mhkit/acoustics/__init__.py
+++ mhkit/acoustics/__init__.py
@@ -1,8 +1,15 @@
 """
-The `acoustics` package of the MHKiT (Marine and Hydrokinetic Toolkit) library
-provides tools and functionalities for analyzing and visualizing passive
-acoustic monitoring data deployed in water bodies. This package reads in raw
-wav files and conducts basic acoustics analysis and visualization.
+The passive acoustics module provides a set of functions
+for analyzing and visualizing passive acoustic monitoring 
+data deployed in water bodies. This package reads in raw
+*.wav* files and conducts basic acoustics analysis and 
+visualization.
+
+To start using the module, import it directly from MHKiT:
+``from mhkit import acoustics``. The analysis functions
+are available directly from the main import, while the 
+I/O and graphics submodules are available from 
+``acoustics.io`` and  ``acoustics.graphics``, respectively.
 """
 
 from mhkit.acoustics import io, graphics
