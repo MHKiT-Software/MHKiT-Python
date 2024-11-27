27114e7e5d35af2b2a41f2351742d5e195e94ca8
ssolson <ssolson@users.noreply.github.com>
2024-11-26 21:59:02 -0500

    Lint: Utils module (#350)
    1. This PR makes the utils module PyLint compatible and enforces this compatibility through GitHub Actions. 
    2. Updates the GitHub Actions testing environment to use the conda environment.yml file for consistency. 
    3. Updates the folium map calls in the Pacwave example to align with the latest folium API changes.

--- mhkit/utils/__init__.py
+++ mhkit/utils/__init__.py
@@ -1,5 +1,12 @@
+"""
+This module initializes and imports the essential utility functions for data 
+conversion, statistical analysis, caching, and event detection for the 
+MHKiT library.
+"""
+
 from .time_utils import matlab_to_datetime, excel_to_datetime, index_to_datetime
 from .stat_utils import (
+    _calculate_statistics,
     get_statistics,
     vector_statistics,
     unwrap_vector,
@@ -15,4 +22,5 @@ from .type_handling import (
     convert_nested_dict_and_pandas,
 )
 
+# pylint: disable=invalid-name
 _matlab = False  # Private variable indicating if mhkit is run through matlab
--- mhkit/utils/type_handling.py
+++ mhkit/utils/type_handling.py
@@ -1,9 +1,27 @@
+"""
+This module provides utility functions for converting various data types
+to xarray structures such as xarray.DataArray and xarray.Dataset. It also
+includes functions for handling nested dictionaries containing pandas 
+DataFrames by converting them to xarray Datasets.
+
+Functions:
+----------
+- to_numeric_array: Converts input data to a numeric NumPy array.
+- convert_to_dataset: Converts pandas or xarray data structures to xarray.Dataset.
+- convert_to_dataarray: Converts various data types to xarray.DataArray.
+- convert_nested_dict_and_pandas: Recursively converts pandas DataFrames 
+  in nested dictionaries to xarray Datasets.
+"""
+
+from typing import Union, Dict, Any
 import numpy as np
 import pandas as pd
 import xarray as xr
 
 
-def to_numeric_array(data, name):
+def to_numeric_array(
+    data: Union[list, np.ndarray, pd.Series, xr.DataArray], name: str
+) -> np.ndarray:
     """
     Convert input data to a numeric array, ensuring all elements are numeric.
     """
@@ -23,12 +41,16 @@ def to_numeric_array(data, name):
     return data
 
 
-def convert_to_dataset(data, name="data"):
+def convert_to_dataset(
+    data: Union[pd.DataFrame, pd.Series, xr.DataArray, xr.Dataset], name: str = "data"
+) -> xr.Dataset:
     """
     Converts the given data to an xarray.Dataset.
 
-    This function is designed to handle inputs that can be either a pandas DataFrame, a pandas Series,
-    an xarray DataArray, or an xarray Dataset. It ensures that the output is consistently an xarray.Dataset.
+    This function is designed to handle inputs that can be either a
+    pandas DataFrame, a pandas Series, an xarray DataArray, or an
+    xarray Dataset. It ensures that the output is consistently an
+    xarray.Dataset.
 
     Parameters
     ----------
@@ -36,14 +58,15 @@ def convert_to_dataset(data, name="data"):
         The data to be converted.
 
     name: str (Optional)
-        The name to assign to the data variable in case the input is an xarray DataArray without a name.
+        The name to assign to the data variable in case the input is an
+        xarray DataArray without a name.
         Default value is 'data'.
 
     Returns
     -------
     xarray.Dataset
-        The input data converted to an xarray.Dataset. If the input is already an xarray.Dataset,
-        it is returned as is.
+        The input data converted to an xarray.Dataset. If the input is
+        already an xarray.Dataset, it is returned as is.
 
     Examples
     --------
@@ -75,7 +98,8 @@ def convert_to_dataset(data, name="data"):
     # Takes data that could be pd.DataFrame, pd.Series, xr.DataArray, or
     # xr.Dataset and converts it to xr.Dataset
     if isinstance(data, pd.DataFrame):
-        # xr.Dataset(data) is drastically faster (1e1 - 1e2x faster) than using pd.DataFrame.to_xarray()
+        # xr.Dataset(data) is drastically faster (1e1 - 1e2x faster)
+        #  than using pd.DataFrame.to_xarray()
         data = xr.Dataset(data)
 
     if isinstance(data, pd.Series):
@@ -86,29 +110,37 @@ def convert_to_dataset(data, name="data"):
 
     if isinstance(data, xr.DataArray):
         # xr.DataArray.to_dataset() breaks if the data variable is unnamed
-        if data.name == None:
+        if data.name is None:
             data.name = name
         data = data.to_dataset()
 
     return data
 
 
-def convert_to_dataarray(data, name="data"):
+def convert_to_dataarray(
+    data: Union[np.ndarray, pd.DataFrame, pd.Series, xr.DataArray, xr.Dataset],
+    name: str = "data",
+) -> xr.DataArray:
     """
     Converts the given data to an xarray.DataArray.
 
-    This function takes in a numpy ndarray, pandas Series, pandas Dataframe, or xarray Dataset
-    and outputs an equivalent xarray DataArray. DataArrays can be passed through with no changes.
+    This function takes in a numpy ndarray, pandas Series, pandas
+    Dataframe, or xarray Dataset and outputs an equivalent xarray
+    DataArray. DataArrays can be passed through with no changes.
 
-    Xarray datasets can only be input when all variable have the same dimensions.
+    Xarray datasets can only be input when all variable have the same
+    dimensions.
 
-    Multivariate pandas Dataframes become 2D DataArrays, which is especially useful when IO
-    functions return Dataframes with an extremely large number of variable. Use the function
-    convert_to_dataset to change a multivariate Dataframe into a multivariate Dataset.
+    Multivariate pandas Dataframes become 2D DataArrays, which is
+    especially useful when IO functions return Dataframes with an
+    extremely large number of variable. Use the function
+    convert_to_dataset to change a multivariate Dataframe into a
+    multivariate Dataset.
 
     Parameters
     ----------
-    data: numpy ndarray, pandas DataFrame, pandas Series, xarray DataArray, or xarray Dataset
+    data: numpy ndarray, pandas DataFrame, pandas Series, xarray
+    DataArray, or xarray Dataset
         The data to be converted.
 
     name: str (Optional)
@@ -118,8 +150,8 @@ def convert_to_dataarray(data, name="data"):
     Returns
     -------
     xarray.DataArray
-        The input data converted to an xarray.DataArray. If the input is already an xarray.DataArray,
-        it is returned as is.
+        The input data converted to an xarray.DataArray. If the input
+        is already an xarray.DataArray, it is returned as is.
 
     Examples
     --------
@@ -152,8 +184,10 @@ def convert_to_dataarray(data, name="data"):
     # Checks pd.DataFrame input and converts to pd.Series if possible
     if isinstance(data, pd.DataFrame):
         if data.shape[1] == 1:
-            # Convert the 1D, univariate case to a Series, which will be caught by the Series conversion below.
-            # This eliminates an unnecessary variable dimension and names the DataArray with the DataFrame variable name.
+            # Convert the 1D, univariate case to a Series, which will
+            # be caught by the Series conversion below. This eliminates
+            # an unnecessary variable dimension and names the DataArray
+            # with the DataFrame variable name.
             #
             # Use iloc instead of squeeze. For DataFrames/Series with only a
             # single value, squeeze returns a scalar which is unexpected.
@@ -161,12 +195,11 @@ def convert_to_dataarray(data, name="data"):
             data = data.iloc[:, 0]
         else:
             # With this conversion, dataframe columns always become "dim_1".
-            # Rename to "variable" to match how multiple Dataset variables get converted into a DataArray dimension
+            # Rename to "variable" to match how multiple Dataset variables get
+            # converted into a DataArray dimension
             data = xr.DataArray(data)
             if data.dims[1] == "dim_1":
-                # Slight chance there is already a name for the columns
                 data = data.rename({"dim_1": "variable"})
-
     # Checks xr.Dataset input and converts to xr.DataArray if possible
     if isinstance(data, xr.Dataset):
         keys = list(data.keys())
@@ -175,33 +208,37 @@ def convert_to_dataarray(data, name="data"):
             data = data[keys[0]]
         else:
             # Allow multiple variables if they have the same dimensions
-            if all([data[keys[0]].dims == data[key].dims for key in keys]):
-                data = (
-                    data.to_array().T
-                )  # transpose so that the new "variable dimension" is the last dimension (matches DataFrame to DataArray behavior)
+            # transpose so that the new "variable dimension" is the last
+            # dimension (matches DataFrame to DataArray behavior)
+            if all(data[keys[0]].dims == data[key].dims for key in keys):
+                data = data.to_array().T
             else:
                 raise ValueError(
-                    "Multivariate Datasets can only be input if all variables have the same dimensions."
+                    "Multivariate Datasets can only be input if all \
+                        variables have the same dimensions."
                 )
 
     # Converts pd.Series to xr.DataArray
     if isinstance(data, pd.Series):
         data = data.to_xarray()
 
-    # Converts np.ndarray to xr.DataArray. Assigns a simple 0-based dimension named index to match how pandas converts to xarray
+    # Converts np.ndarray to xr.DataArray. Assigns a simple 0-based
+    # dimension named index to match how pandas converts to xarray
     if isinstance(data, np.ndarray):
         data = xr.DataArray(
             data=data, dims="index", coords={"index": np.arange(len(data))}
         )
 
-    # If there's no data name, add one to prevent issues calling or converting to a Dataset later on
-    if data.name == None:
-        data.name = name
+    # If there's no data name, add one to prevent issues calling or
+    # converting to a Dataset later on
+    data.name = data.name if data.name is not None else name
 
     return data
 
 
-def convert_nested_dict_and_pandas(data):
+def convert_nested_dict_and_pandas(
+    data: Dict[str, Union[pd.DataFrame, Dict[str, Any]]]
+) -> Dict[str, Union[xr.Dataset, Dict[str, Any]]]:
     """
     Recursively searches inside nested dictionaries for pandas DataFrames to
     convert to xarray Datasets. Typically called by wave.io functions that read
