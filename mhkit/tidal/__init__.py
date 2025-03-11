3a4751cd798917d5e18a5f84a2cb282c6e3f9625
ssolson <sterlingolson@yahoo.com>
2025-03-11 12:22:17 -0400

    linted graphics

--- mhkit/tidal/__init__.py
+++ mhkit/tidal/__init__.py
@@ -1,3 +1,9 @@
+"""
+MHKiT Tidal Module
+
+This module provides tools for analyzing and processing tidal data.
+"""
+
 from mhkit.tidal import graphics
 from mhkit.tidal import io
 from mhkit.tidal import resource
