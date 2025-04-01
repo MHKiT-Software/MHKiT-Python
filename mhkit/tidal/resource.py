5f4d6b255693a8341bfdb3efd224020706a266c8
ssolson <sterlingolson@yahoo.com>
2025-04-01 12:41:28 -0400

    froude_number

--- mhkit/tidal/resource.py
+++ mhkit/tidal/resource.py
@@ -30,12 +30,12 @@ Functions:
 
 import math
 import numpy as np
-from mhkit.river.resource import exceedance_probability, Froude_number
+from mhkit.river.resource import exceedance_probability, froude_number
 from mhkit.utils import convert_to_dataarray
 
 __all__ = [
     "exceedance_probability",
-    "Froude_number",
+    "froude_number",
     "principal_flow_directions",
     "_histogram",
     "_flood_or_ebb",
