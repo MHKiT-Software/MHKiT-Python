242826edfa58fed7432ee12bdd99dc6fe6368d2d
ssolson <ssolson@users.noreply.github.com>
2025-04-03 10:42:01 -0600

    Update mhkit/tidal/resource.py
    Co-authored-by: Adam Keester <72414466+akeeste@users.noreply.github.com>

--- mhkit/tidal/resource.py
+++ mhkit/tidal/resource.py
@@ -10,22 +10,6 @@ Features:
 - Determines principal flow directions using histogram-based analysis.
 - Classifies ebb and flood conditions based on directional data.
 - Normalizes flow angles for consistent analysis.
-
-Functions:
-----------
-- `_histogram(directions, velocities, width_dir, width_vel)`:
-  Computes a joint probability histogram of flow directions and velocities.
-
-- `_normalize_angle(degree)`:
-  Normalizes an angle to the range [0, 360] degrees.
-
-- `principal_flow_directions(directions, width_dir)`:
-  Determines principal flow directions for ebb and flood cycles
-  using a histogram-based approach.
-
-- `_flood_or_ebb(d, flood, ebb)`:
-  Identifies whether a given set of flow directions corresponds to
-  ebb or flood conditions.
 """
 
 import math
