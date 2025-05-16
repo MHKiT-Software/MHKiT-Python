5d168db9d647b4af2484ea6d9638b08595e2ea84
ssolson <sterlingolson@yahoo.com>
2025-05-16 11:18:09 -0400

    fix typos

--- mhkit/wave/io/hindcast/__init__.py
+++ mhkit/wave/io/hindcast/__init__.py
@@ -1,8 +1,8 @@
-"""
+"""Wave hindcast data import and processing module.
 
 This module provides functionality for importing and processing wave hindcast data,
-including wind toolkit data and WPTO hindcast data. THe hindcast io module is
-seperated from the general geio module to allow for more efficient handling of
+including wind toolkit data and WPTO hindcast data. The hindcast io module is
+separated from the general io module to allow for more efficient handling of
 CI tests.
 """
 
