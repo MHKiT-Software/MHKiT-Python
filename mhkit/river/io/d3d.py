bb1f2182ac063051b3f23002461dc74369c421d4
Emily Browning <eabrowning@alaska.edu>
2025-12-01 11:01:39 -0900

    black formatting

--- mhkit/river/io/d3d.py
+++ mhkit/river/io/d3d.py
@@ -894,7 +894,11 @@ def list_variables(data: Union[netCDF4.Dataset, xr.Dataset, xr.DataArray]) -> Li
         "data must be a NetCDF4 Dataset, xarray Dataset, or "
         f"xarray DataArray. Got: {type(data)}"
     )
-def calculate_grid_convergence_index(fine_grid, coarse_grid, refinement_ratio,factor_of_safety=1.25, order=2):
+
+
+def calculate_grid_convergence_index(
+    fine_grid, coarse_grid, refinement_ratio, factor_of_safety=1.25, order=2
+):
     """
     Calculate the Grid Convergence Index (GCI) between two grid sizes. https://www.grc.nasa.gov/WWW/wind/valid/tutorial/spatconv.html
 
@@ -904,7 +908,7 @@ def calculate_grid_convergence_index(fine_grid, coarse_grid, refinement_ratio,fa
         Results from the finer grid.
     coarse_grid: numpy.ndarray
         Results from the coarser grid.
-    refinement_ratio: float 
+    refinement_ratio: float
         Refinement ratio between the grids.
     order: int
         Order of accuracy (default is 2).
