Updates for DOLfYN: improved handling of "averaged" profiles, ADCP waves example, critical PSD bugfix (#430)
Collection of bugfixes and documentation for Nortek Signature ADCPs.

I received a dual-profile datafile from a Nortek Signature250 deployed
at PacWave that was collecting both water velocity and wave
measurements, and I made some updates to the codebase when things would
fail working through the standard ADCP workflow.

DOLfYN is currently set up to return individual profiles as individual
datasets, so there is one dataset containing the wave-relevant
variables, and one containing the water velocity variables. Both
datasets contain water velocity information, so to differentiate between
the two, the "_avg" tag is added to the dataset containing what Nortek
calls the "averaging" profile. However, this dataset no longer comes out
of the box "bin-averaged"; individual pings from each duty cycle are now
saved in the output file.

I've been making updates to dolfyn so that the codebase will recognize
"_avg" variables and default to them if untagged variables do not exist.
The following updates are the latest:
- The "U_mag" and "U_dir" shortcuts will using "vel_avg" to calculate
speed and direction if "vel" does not exist in the dataset.
- The shear functions in the ADCP turbulence API (`dudz`, `dvdz`, etc)
can now utilize "vel_avg" if given as an input.
- `calc_declination` will no longer complain if you try to update the
magnetic declination in the Nortek-created bin-averaged binary file
("<filename>_avgd.ad2cp")

Two, I created an example notebook showing how to calculate wave
statistics using DOLfYN's FFT tools. We've gotten questions about this
in the past, and now that I have a good dataset, this is a good time to
document this.

Three, I found a bug in the PSD functions where individual FFTs get a
50% overlap not once, but twice. The core FFT function applies a 50%
overlap using a series of "for loops" (the more robust method), while
the input FFT function 

mhkit/dolfyn/binned.py
@@ -370,7 +370,6 @@ class TimeBinner:
         noise=0,
         n_bin=None,
         n_fft=None,
-        n_pad=None,
         step=None,
     ):
         """
@@ -393,8 +392,6 @@ class TimeBinner:
         n_fft : int
           n_fft of veldat2, number of elements per bin if 'None' is taken
           from VelBinner
-        n_pad : int (optional)
-          The number of values to pad with zero. Default = 0
         step : int (optional)
           Controls amount of overlap in fft. Default: the step size is
           chosen to maximize data use, minimize nens, and have a
@@ -413,11 +410,9 @@ class TimeBinner:
         fs = self._parse_fs(fs)
         n_bin = self._parse_nbin(n_bin)
         n_fft = self._parse_nfft(n_fft)
-        if n_pad is None:
-            n_pad = min(n_bin - n_fft, n_fft)
         out = np.empty(self._outshape_fft(dat.shape, n_fft=n_fft, n_bin=n_bin))
         # The data is detrended in psd, so we don't need to do it here.
-        dat = self.reshape(dat, n_pad=n_pad)
+        dat = self.reshape(dat)
 
         for slc in slice1d_along_axis(dat.shape, -1):
             out[slc] = psd_1D(dat[slc], n_fft, fs, window=window, step=step)
@@ -474,8 +469,8 @@ class TimeBinner:
         oshp[-2] = np.min([oshp[-2], int(dat2.shape[-1] // n_bin2)])
 
         # The data is detrended in psd, so we don't need to do it here:
-        dat1 = self.reshape(dat1, n_pad=n_fft)
-        dat2 = self.reshape(dat2, n_pad=n_fft)
+        dat1 = self.reshape(dat1)
+        dat2 = self.reshape(dat2)
         out = np.empty(oshp, dtype="c{}".format(dat1.dtype.itemsize * 2))
         if dat1.shape == dat2.shape:
             cross = cpsd_1D